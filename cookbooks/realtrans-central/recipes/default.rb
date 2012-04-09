14e837284464790e6196c2ef8764467e116fd3d0
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-09 10:41:52 -0400

    Fixed variable names

--- cookbooks/realtrans-central/recipes/default.rb
+++ cookbooks/realtrans-central/recipes/default.rb
@@ -7,13 +7,13 @@
 app_name = "realtrans-central"
 include_recipe "altitomcat"
 
-package {app_name} do
-  version "node[{app_name}_version].noarch"
+package "#{app_name}" do
+  version "node[#{app_name}_version].noarch"
   action :install
 end
 
-template "/opt/tomcat/conf/{app_name}.properties" do
-  source "{app_name}.properties.erb"
+template "/opt/tomcat/conf/#{app_name}.properties" do
+  source "#{app_name}.properties.erb"
   group 'tomcat'
   owner 'tomcat'
   mode '0644'
