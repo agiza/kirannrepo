6a6b923ce88df6cbb017c025e87ff23b39193a85
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-09 08:35:59 -0400

    Genericize recipe

--- cookbooks/int-datavision/recipes/default.rb
+++ cookbooks/int-datavision/recipes/default.rb
@@ -6,15 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+app_name = "int-datavision"
 include_recipe "altitomcat"
 
-package "int-datavision" do
-  version "node[int_datavision_version].noarch"
+package {app_name} do
+  version "node[{app_name}_version].noarch"
   action :install
 end
 
-template "/opt/tomcat/conf/int-datavision.properties" do
-  source "int-datavision.properties.erb"
+template "/opt/tomcat/conf/{app_name}.properties" do
+  source "{app_Name}.properties.erb"
   group 'tomcat'
   owner 'tomcat'
   mode '0644'
