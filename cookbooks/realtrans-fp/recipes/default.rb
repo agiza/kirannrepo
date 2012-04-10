579a2ceb5d2bcb469e5a700cddd42ad471e63a66
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-10 10:03:16 -0400

    Fixing version

--- cookbooks/realtrans-fp/recipes/default.rb
+++ cookbooks/realtrans-fp/recipes/default.rb
@@ -7,10 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 app_name = "realtrans-fp"
+app_version = node[:realtrans-fp_version]
+
 include_recipe "altitomcat"
 
 package "#{app_name}" do
-  version "node[#{app_name}_version].noarch"
+  version "#{app_version"
   action :install
 end
 
