3fcb55602286ed6ebf14f54a293a2918893e0863
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-10 10:01:35 -0400

    Fixing version

--- cookbooks/int-interthinx/recipes/default.rb
+++ cookbooks/int-interthinx/recipes/default.rb
@@ -7,10 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 app_name = "int-interthinx"
+app_version = node[:int-interthinx_version"
+
 include_recipe "altitomcat"
 
 package "#{app_name}" do
-  version "node[#{app_name}_version].noarch"
+  version "#{app_version}"
   action :install
 end
 
