7e81b7d4293809849c303fa4f35e8964a669122e
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-11 09:58:16 -0400

    Trying to fix the execution portion

--- cookbooks/altirepo/recipes/default.rb
+++ cookbooks/altirepo/recipes/default.rb
@@ -7,14 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-execute "yum_clean" do
-  command "yum clean all"
-  action "nothing"
-end
-
 template "/etc/yum.repos.d/altisource.repo" do
   source "altisource.repo.erb"
   mode "0644"
-  notifies :run, resources(:execute => "yum_clean")
 end
 
+execute "yum" do
+  command "yum clean all"
+  action ":run"
+end
