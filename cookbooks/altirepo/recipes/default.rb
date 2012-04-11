1526c64d5bf3511ab714d80edd73084702f4f337
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-11 10:07:29 -0400

    Trying to fix this so that changes to repo file will cause a yum clean

--- cookbooks/altirepo/recipes/default.rb
+++ cookbooks/altirepo/recipes/default.rb
@@ -7,12 +7,14 @@
 # All rights reserved - Do Not Redistribute
 #
 
+execute "yum" do
+  command "yum clean all"
+  action :nothing
+end
+
 template "/etc/yum.repos.d/altisource.repo" do
   source "altisource.repo.erb"
   mode "0644"
+  notifies :run, resources(:execute => "yum")
 end
 
-execute "yum" do
-  command "yum clean all"
-  action "run"
-end
