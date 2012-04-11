aba764fe6c3f3a22ae8770db79d54708899e4e04
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-11 11:30:47 -0400

    Added recipe for realfoundationapp

--- cookbooks/realfoundationapp/recipes/default.rb
+++ cookbooks/realfoundationapp/recipes/default.rb
@@ -2,7 +2,25 @@
 # Cookbook Name:: realfoundationapp
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+app_name = "realfoundationapp"
+app_version = node[:realfoundationapp_version]
+
+include_recipe "altitomcat"
+
+package "#{app_name}" do
+  version "#{app_version}"
+  action :install
+end
+
+template "/opt/tomcat/conf/realfoundationapp.properties" do
+  source "realfoundationapp.properties.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
+
