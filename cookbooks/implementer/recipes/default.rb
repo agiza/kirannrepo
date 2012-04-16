16a0442a2d64f2a487f75757ac5d9bfeb6feb68e
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-16 14:40:48 -0400

    Updated to install implementer application but then remove recipe from run_list

--- cookbooks/implementer/recipes/default.rb
+++ cookbooks/implementer/recipes/default.rb
@@ -2,7 +2,23 @@
 # Cookbook Name:: implementer
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+app_name = "implementer"
+app_version = node[:implementer_version]
+
+include_recipe "altitomcat"
+
+package "#{app_name}" do
+  version "#{app_version}"
+  action :install
+end
+
+ruby_block "remove implementer from run list" do
+  block do
+    node.run_list.remove("recipe[implementer]")
+  end
+end
+
