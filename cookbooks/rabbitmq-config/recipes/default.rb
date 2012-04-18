a0df9c8643d34db5c279b5f6f7fac7114fcd86ff
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-18 08:47:27 -0400

    Adding recipe for rabbitmq-server configuration rpm

--- cookbooks/rabbitmq-config/recipes/default.rb
+++ cookbooks/rabbitmq-config/recipes/default.rb
@@ -2,7 +2,43 @@
 # Cookbook Name:: rabbitmq-config
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "altirepo"
+
+app_name = "rabbitmq-server-config"
+app_version = node[:rabbitmq-config_version]
+
+package "rabbitmq-server" do
+  version "2.7.1-1"
+  provider Chef::Provider::Package::Yum
+  action :install
+end
+
+package "#{app_name}" do
+  version "#{app_version}"
+  provider Chef::Provider::Package::Yum
+  action :install
+end
+
+service "rabbitmq-server" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :enable
+end
+
+template "/etc/rabbitmq/rabbitmq.config" do
+  source "rabbitmq.config.erb"
+  group 'root'
+  owner 'root'
+  mode '0644'
+  notifies :restart, resources(:service => "rabbitmq-server")
+end
+
+execute "queue-config" do
+  command "/etc/rabbitmq/realtrans-rabbit.sh"
+  action :run
+  environment ({'HOME' => '/etc/rabbitmq'})
+end
+
