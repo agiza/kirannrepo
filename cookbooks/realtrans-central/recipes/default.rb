a29e6f96b839b123894e797a05552029958d7b86
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-24 23:38:14 +0000

    Trying to add logic to remove the old package before installing new version, then restart tomcat

--- cookbooks/realtrans-central/recipes/default.rb
+++ cookbooks/realtrans-central/recipes/default.rb
@@ -10,6 +10,10 @@ app_version = node[:realtranscentral_version]
 include_recipe "altitomcat"
 
 package "#{app_name}" do
+  action :remove
+end
+
+package "#{app_name}" do
   version "#{app_version}"
   action :install
 end
@@ -20,5 +24,9 @@ template "/opt/tomcat/conf/#{app_name}.properties" do
   owner 'tomcat'
   mode '0644'
   notifies :restart, resources(:service => "altitomcat")
+end
+
+service "altitomcat" do
+  action :start
 end
 
