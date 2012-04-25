a9d5524e959a53e7978013cb3592c0d5e2e52100
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-25 10:51:46 -0400

    Moving altitomcat service definition

--- cookbooks/altitomcat/recipes/default.rb
+++ cookbooks/altitomcat/recipes/default.rb
@@ -7,15 +7,15 @@
 app_name = "altitomcat"
 app_version = node[:altitomcat_version]
 
+service "altitomcat" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :enable
+end
+
 package "#{app_name}" do
   version "#{app_version}.noarch"
   action :install
   notifies :restart, resources(:service => "altitomcat")
-end
-
-service "altitomcat" do
-  supports :stop => true, :start => true, :restart => true, :reload => true
-  action :enable
 end
 
 template "/opt/tomcat/bin/catalina.sh" do
