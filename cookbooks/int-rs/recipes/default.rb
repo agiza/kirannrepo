14f614d6cdc3d6ca05366994cbcfad42654d6411
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-26 07:29:17 -0400

    Changes for all recipes and adding int-rs and int-rres

--- cookbooks/int-rs/recipes/default.rb
+++ cookbooks/int-rs/recipes/default.rb
@@ -2,7 +2,33 @@
 # Cookbook Name:: int-rs
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+app_name = "int-rs"
+app_version = node[:intrs_version]
+
+include_recipe "altitomcat"
+
+service "altitomcat" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+yum_package "#{app_name}" do
+  version "#{app_version}"
+  action :install
+  flush_cache [ :before ]
+  allow_downgrade true
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/#{app_name}.properties" do
+  source "#{app_name}.properties.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
+
