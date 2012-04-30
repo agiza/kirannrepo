d84be991d7927c391d2dae058241121a47102d0e
Bryan Phinney <bryan.phinney@altisource.com>
2012-04-30 09:29:19 -0400

    Updated for all the current haproxy servers

--- cookbooks/haproxy/recipes/default.rb
+++ cookbooks/haproxy/recipes/default.rb
@@ -2,7 +2,27 @@
 # Cookbook Name:: haproxy
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+app_name = "haproxy"
+
+service "haproxy" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :enable
+end
+
+yum_package "#{app_name}" do
+  action :update
+  notifies :restart, resources(:service => "haproxy")
+end
+
+template "/etc/haproxy/haproxy.cfg" do
+  source "haproxy.cfg.erb"
+  group "haproxy"
+  owner "haproxy"
+  mode "0644"
+  notifies :restart, resources(:service => "haproxy")
+end
+
