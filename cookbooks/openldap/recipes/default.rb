9cb0ee303939acb29968a08e06214b6915d18868
Bryan Phinney <bryan.phinney@altisource.com>
2012-05-03 09:42:14 -0400

    Updated for openldap for Redhat

--- cookbooks/openldap/recipes/default.rb
+++ cookbooks/openldap/recipes/default.rb
@@ -2,7 +2,18 @@
 # Cookbook Name:: openldap
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+
+service "slapd" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :enable
+end
+
+package "openldap-servers" do
+  action :update
+  notifies :restart, resources(:service => "slapd")
+end
+
