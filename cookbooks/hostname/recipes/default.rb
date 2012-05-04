38d0a080b5325242f0956dcc021ffb1b25cde4fe
Bryan Phinney <bryan.phinney@altisource.com>
2012-05-04 08:10:41 -0400

    Creating a recipe to install a script to set the hostname

--- cookbooks/hostname/recipes/default.rb
+++ cookbooks/hostname/recipes/default.rb
@@ -2,7 +2,21 @@
 # Cookbook Name:: hostname
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+
+execute "chkconfig" do
+  command "chkconfig --add host_command; chkconfig host_command on"
+  action :nothing
+end
+
+template "/etc/init.d/host_command" do
+  source "host_command.erb"
+  mode "0755"
+  owner "root"
+  group "root"
+  notifies :run, resources(:execute => "chkconfig")
+end
+
