46ba237fb338082574bf48d82d83adb66d1fd167
Bryan Phinney <bryan.phinney@altisource.com>
2012-05-04 09:01:33 -0400

    Updated again

--- cookbooks/hostname/recipes/default.rb
+++ cookbooks/hostname/recipes/default.rb
@@ -7,16 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 
-execute "update-rc" do
-  command "sudo update-rc.d host_command defaults; sudo update-rc.d host_command enable"
-  action :nothing
-end
-
-execute "chkconfig" do
-  command "chkconfig --add host_command; chkconfig host_command on"
-  action :nothing
-end
-
 template "/etc/init.d/host_command" do
   source "host_command.erb"
   mode "0755"
@@ -25,10 +15,16 @@ template "/etc/init.d/host_command" do
 end
 
 if platform?("redhat", "centos", "fedora")
-  notifies :run, resources(:execute => "chkconfig")
+  execute "chkconfig" do
+    command "chkconfig --add host_command; chkconfig host_command on"
+    action :run
+  end
 end
 
 if platform?("ubuntu")
-   notifies :run, resources(:execute => "update-rc")
+  execute "update-rc" do
+    command "sudo update-rc.d host_command defaults; sudo update-rc.d host_command enable"
+    action :run
+  end
 end
 
