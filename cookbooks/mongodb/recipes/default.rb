b50151ed566eabaf3fdb8340c7b3c00e9f34c70a
Bryan Phinney <bryan.phinney@altisource.com>
2012-05-07 08:37:50 -0400

    Adding new info for this recipe

--- cookbooks/mongodb/recipes/default.rb
+++ cookbooks/mongodb/recipes/default.rb
@@ -2,7 +2,27 @@
 # Cookbook Name:: mongodb
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+include_recipe "altirepo::aegisco"
+
+package "mongo-10gen-server" do
+  version "2.0.4"
+  action :install
+end
+
+service "mongod" do
+  supports :stop => true, :start => true, :restart => true, :status => true, :reload => true
+  action :enable
+end
+
+template "/etc/mongod.conf" do
+  source "mongod.conf.erb"
+  group "root"
+  owner "root"
+  mode "0644"
+  notifies :reload, resources(:service => "mongod")
+end
+
