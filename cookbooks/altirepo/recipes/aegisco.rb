8ed8ace0e018ad8b46204e6f573e18f4b9f06b6c
Bryan Phinney <bryan.phinney@altisource.com>
2012-05-07 08:23:25 -0400

    Adding recipe for aegisco repository, used for mongodb

---
+++ cookbooks/altirepo/recipes/aegisco.rb
@@ -0,0 +1,18 @@
+# Recipe:: aegisco
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "yum" do
+  command "yum clean all"
+  action :nothing
+end
+
+template "/etc/yum.repos.d/aegisco.repo" do
+  source "aegisco.repo.erb"
+  mode "0644"
+  notifies :run, resources(:execute => "yum")
+end
+
