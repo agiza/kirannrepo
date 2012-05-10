656902068fec38dfba641ff252c831ea529ed604
Bryan Phinney <bryan.phinney@altisource.com>
2012-05-10 14:26:18 -0400

    Updated recipe for package, service and template for configuration

--- cookbooks/elasticsearch/recipes/default.rb
+++ cookbooks/elasticsearch/recipes/default.rb
@@ -2,7 +2,25 @@
 # Cookbook Name:: elasticsearch
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+package "elasticsearch" do
+  action :upgrade
+end
+
+service "elasticsearch" do
+  supports :start => true, :stop => true, :restart => true, :reload => true
+  action :enable
+  action :start
+end
+
+template "/etc/sysconfig/sysconfig-elastisearch" do
+  source "sysconfig-elasticsearch.erb"
+  owner  "root"
+  group  "root"
+  notifies :restart, resources(:service => "elasticsearch")
+end
+
+
