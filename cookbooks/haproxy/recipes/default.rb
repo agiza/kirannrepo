7deedf99c4ca01b3b6c8c7746755e345879e7e7f
Bryan Phinney <bryan.phinney@altisource.com>
2012-05-10 14:53:44 +0000

    Updated to remove extraneous characters

--- cookbooks/haproxy/recipes/default.rb
+++ cookbooks/haproxy/recipes/default.rb
@@ -18,9 +18,10 @@ service "haproxy" do
 end
 
 rabbitservers = search(:node, "role:rabbitserver AND chef_environment:#{node.chef_environment}")
-clusternodes = rabbitservers.collect { |rabbitserver| "rabbit@#{rabbitserver}" }.join(" ")
+clusternodes = rabbitservers.collect { |rabbitserver| "#{rabbitserver}" }.join(" ")
 clusternodes = clusternodes.gsub!("node\[", "")
 clusternodes = clusternodes.gsub!("\]", "")
+clusternodes = clusternodes.gsub!(".altidev.com", "")
 
 template "/etc/haproxy/haproxy.cfg" do
   source "haproxy.cfg.erb"
