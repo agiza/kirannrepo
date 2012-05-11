caf4c6284323227dde403060088a7ded81ac6c05
Bryan Phinney <bryan.phinney@altisource.com>
2012-05-11 08:04:37 -0400

    Adding templates and recipe to manage realtrans app configurations

--- cookbooks/realtrans-config/recipes/default.rb
+++ cookbooks/realtrans-config/recipes/default.rb
@@ -2,7 +2,38 @@
 # Cookbook Name:: realtrans-config
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+
+service "altitomcat" do
+  supports :start => true, :stop => true, :restart => true, :status => true
+  action :nothing
+end
+
+template "/opt/tomcat/conf/realtrans-central.properties" do
+  source "realtrans-central.properties.erb"
+  owner  "tomcat"
+  group  "tomcat"
+  mode   "0644"
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/realtrans-rp.properties" do
+  source "realtrans-rp.properties.erb"
+  owner  "tomcat"
+  group  "tomcat"
+  mode   "0644"
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/realtrans-fp.properties" do
+  source "realtrans-fp.properties.erb"
+  owner  "tomcat"
+  group  "tomcat"
+  mode   "0644"
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+
