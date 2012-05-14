d7336e492911655fcb1ef4e725941f213658e695
Bryan Phinney <bryan.phinney@altisource.com>
2012-05-14 15:29:09 +0000

    Updated to add space

--- cookbooks/bamboo-server/recipes/default.rb
+++ cookbooks/bamboo-server/recipes/default.rb
@@ -12,6 +12,7 @@ service "bamboo" do
   action :enable
   action :start
 end
+
 bambooLicenseKey = 'AAABMA0ODAoPeNptkF1rgzAUhu/zKwK7TjG2lrUQWNUw3NQWbdnNbqI7WwM1ShLL/PdLbbuPMkhu8vC+5zm5K3uFMzFgOnVnSe+XdIGjeIt9j/ooBlNr2VnZKhaKpmpbzJUF3Wlp4HWJ+VEcenHCKO+bCvT6fWdAG+YHKNIwklhYYKc24gWEzlDUKitqm4sGWKgHofBmL5WC4Up4JuSBVSc06c7oQRysNG2va5jUbYN+5jKre0DV6DZxYXmE85MrcaZKqBr4Zyf18MtjTuj0mkllDcoAf5NjHc+3vNgUScnRWn8IJc15zOpbAJU8Z+6SlM7pwgvmqAR9BJ3ELHx8WZHnMvDIbvbkk8QPy5sx26GDcfNonWW8iJJVii7I5dMk/lN8dfvXf+Nk9sLA7f9+AWwPmK8wLAIUVNr6ciLulZ0lzzzqLzHvN0ApQ5YCFAzRP0i6evwDqsoYujmKVmwtcwcUX02f7'
 template "/opt/atlassian/bamboo/bamboo.cfg.xml" do
   source "bamboo.cfg.xml.erb"
