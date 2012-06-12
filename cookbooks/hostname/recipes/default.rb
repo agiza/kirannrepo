f809741fa8d762b92e02b2a21b1d73066f4cd9fe
Bryan Phinney <bryan.phinney@altisource.com>
2012-06-12 13:39:00 -0400

    Working with the order to try to get init scripts to run

--- cookbooks/hostname/recipes/default.rb
+++ cookbooks/hostname/recipes/default.rb
@@ -19,26 +19,23 @@ execute "hostname" do
   end
 end
 
-execute "chkconfig" do
-  command " chkconfig host_command enable"
-  action :nothing
-end
-
-execute "update-rc.d" do
-  command "update-rc.d host_command defaults; update-rc.d host_command enable"
-  action :nothing
-end
-
 template "/etc/init.d/host_command" do
   source "host_command.erb"
   mode "0755"
   owner "root"
   group "root"
-  case node[:platform]
-  when "centos", "redhat", "fedora", "suse"
-    notifies :run, resources(:execute => "chkconfig")
-  when "debian", "ubuntu"
-    notifies :run, resources(:execute => "update-rc.d")
+end
+
+case node[:platform]
+when "centos", "redhat", "fedora", "suse"
+  execute "chkconfig" do
+    command " chkconfig host_command enable"
+    action :run
+  end
+when "debian", "ubuntu"
+  execute "update-rc.d" do
+    command "update-rc.d host_command defaults; update-rc.d host_command enable"
+    action :run
   end
 end
 
