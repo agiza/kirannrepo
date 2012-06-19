6ba601afe2063111206ac9db848c7e59ff614ca4
Bryan Phinney <bryan.phinney@altisource.com>
2012-06-19 11:01:08 -0400

    Adding the files for splunkforwarder recipe

--- cookbooks/splunkforwarder/recipes/default.rb
+++ cookbooks/splunkforwarder/recipes/default.rb
@@ -2,7 +2,37 @@
 # Cookbook Name:: splunkforwarder
 # Recipe:: default
 #
-# Copyright 2012, YOUR_COMPANY_NAME
+# Copyright 2012, Altisource
 #
 # All rights reserved - Do Not Redistribute
 #
+
+service "splunkforwarder" do
+  supports :stop => true, :start => true, :reload => true
+  action :nothing
+end
+
+template "/opt/splunkforwarder/etc/system/local/server.conf" do
+  source "server.conf.erb"
+  owner  "splunk"
+  group  "splunk"
+  mode   "0644"
+  notifies :restart, resources(:service => "splunkforwarder")
+end
+
+template "/opt/splunkforwarder/etc/system/local/outputs.conf" do
+  source "outputs.conf.erb"
+  owner  "splunk"
+  group  "splunk"
+  mode   "0644"
+  notifies :restart, resources(:service => "splunkforwarder")
+end
+
+template "/opt/splunkforwarder/etc/apps/search/local/inputs.conf" do
+  source "inputs.conf.erb"
+  owner  "splunk"
+  group  "splunk"
+  mode   "0644"
+  notifies :restart, resources(:service => "splunkforwarder")
+end
+
