2c1ea0eb7eca85036811e057b81bb4f7cb944eeb
Bryan Phinney <bryan.phinney@altisource.com>
2012-06-20 16:32:22 +0000

    Updated to try to handle starting splunkforwarder

--- cookbooks/splunkforwarder/recipes/default.rb
+++ cookbooks/splunkforwarder/recipes/default.rb
@@ -24,7 +24,7 @@ execute "splunk-first-run" do
 end
 
 execute "splunk-boot-enable" do
-  command "/opt/splunkforwarder/bin/splunk start; /opt/splunkforwarder/bin/splunk enable boot-start -user splunk"
+  command "/opt/splunkforwarder/bin/splunk status > /dev/null; splunk_check=`echo $?`; if [ "$splunk_check" == "3" ]; then /opt/splunkforwarder/bin/splunk start; fi; /opt/splunkforwarder/bin/splunk enable boot-start -user splunk"
   action :run
 end
 
