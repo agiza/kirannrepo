ae37835788d127293e382088e192239d82ffda8f
Bryan Phinney <bryan.phinney@altisource.com>
2012-06-25 14:56:27 -0400

    New cookbook for realtrans-vp

---
+++ cookbooks/realtrans-vp/recipes/default.rb
@@ -0,0 +1,42 @@
+#
+# Cookbook Name:: realtrans-vp
+# Recipe:: default
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+app_name = "realtrans-vp"
+app_version = node[:realtransfp_version]
+
+include_recipe "altitomcat"
+
+service "altitomcat" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+yum_package "#{app_name}" do
+  version "#{app_version}"
+  action :install
+  flush_cache [ :before ]
+  allow_downgrade true
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/#{app_name}.properties" do
+  source "#{app_name}.properties.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/Catalina/localhost/#{app_name}.xml" do
+  source "#{app_name}.xml.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
+
