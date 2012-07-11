6e4265852b226f48b2e4f4227688bc42b860e7f3
Bryan Phinney <bryan.phinney@altisource.com>
2012-07-11 10:26:47 -0400

    Updated to bring properties in line with version 0.6.0 for next release

---
+++ cookbooks/realtrans-reg/recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: realtrans-reg
+# Recipe:: default
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+app_name = "realtrans-reg"
+app_version = node[:realtransreg_version]
+
+include_recipe "altitomcat"
+
+service "altitomcat" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+yum_package "#{app_name}" do
+  version "#{app_version}"
+  action :install
+  flush_cache [ :before ]
+  allow_downgrade true
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/realtrans-reg.properties" do
+  source "realtrans-reg.properties.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/Catalina/localhost/realtrans-reg.xml" do
+  source "realtrans-reg.xml.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
