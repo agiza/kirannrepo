5f8493d8ffcc77b2ef90e93d878525e75f874a4e
Bryan Phinney <bryan.phinney@altisource.com>
2012-07-20 14:20:20 -0400

    Added recipe for realdoc

---
+++ cookbooks/realdoc/recipes/default.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: realdoc
+# Recipe:: default
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+app_name = "realdoc"
+app_version = node[:realdoc_version]
+
+include_recipe "altitomcat"
+
+service "altitomcat" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+yum_package "#{app_name}" do
+  version "#{app_version}"
+  action :install
+  flush_cache [ :before ]
+  allow_downgrade true
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/realdoc.properties" do
+  source "realdoc.properties.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/Catalina/localhost/realdoc.xml" do
+  source "realdoc.xml.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
