ccdf61dd434fd8d0bafd6397a268327c8084fefd
Bryan Phinney <bryan.phinney@altisource.com>
2012-08-01 09:45:43 -0400

    Updating apache recipe to push toward a CentOS system.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -7,276 +7,140 @@
 # All rights reserved - Do Not Redistribute
 #
 
-package "apache2" do
-  case node[:platform]
-  when "centos","redhat","fedora","suse"
-    package_name "httpd"
-  when "debian","ubuntu"
-  package_name "apache2"
-  end
+yum_package "httpd" do
   action :upgrade
 end
 
-package "mod_security" do
-  case node[:platform]
-  when "centos","redhat","fedora","suse"
-    package_name "mod_security"
-  when "debian","ubuntu"
-    package_name "libapache2-modsecurity"
-  end
+yum_package "mod_security" do
   action :upgrade
 end
 
-service "apache2" do
-  case node[:platform]
-  when "centos","redhat","fedora","suse"
-    service_name "httpd"
-  when "debian","ubuntu"
-    service_name "apache2"
-  end
+service "httpd" do
   supports :stop => true, :start => true, :restart => true, :reload => true
   action [:enable, :start]
 end
 
-case node[:platform]
-  when "centos","redhat","fedora","suse"
-    template "/etc/httpd/conf/httpd.conf" do
-      source "httpd.rhel.conf.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      notifies :reload, resources(:service => "httpd")
-    end
-  when "debian","ubuntu"
-    template "/etc/apache2/httpd.conf" do
-      source "httpd.conf.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      notifies :reload, resources(:service => "apache2")
-    end
+directory "/var/www/html/demo"
+  owner  "root"
+  group  "root"
 end
 
-case node[:platform]
-  when "debian","ubuntu"
-    directory "/etc/apache2/proxy-available" do
-      owner "root"
-      group "root"
-    end
-
-    directory "/etc/apache2/proxy-enabled" do
-      owner "root"
-      group "root"
-    end
-
-    template "/etc/apache2/ports.conf" do
-      source "ports.conf.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      notifies :reload, resources(:service => "apache2")
-    end
-
-    template "/etc/apache2/apache2.conf" do
-      source "apache2.conf.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      notifies :reload, resources(:service => "apache2")
-    end
-
-    template "/etc/apache2/conf.d/security" do
-      source "security.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      notifies :reload, resources(:service => "apache2")
-    end
-
-    template "/etc/apache2/mods-available/000-default" do
-      source "default.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      notifies :reload, resources(:service => "apache2")
-    end
-
-    template "/etc/apache2/mods-available/000-default-ssl" do
-      source "default-ssl.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      notifies :reload, resources(:service => "apache2")
-    end
-   
-    template "/etc/apache2/proxy-available/rtqaproxy.conf" do
-      source "rtqaproxy.conf.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      variables(:servers => server_list)
-      notifies :reload, resources(:service => "apache2")
-    end
-
-    template "/etc/apache2/proxy_available/rtdemoproxy.conf" do
-      source "rtdemoproxy.conf.erb"
-      owner "root"
-      group "root"
-      mode "0644"
-      variables(:servers => server_list)
-      notifies :reload, resources(:service => "apache2")
-    end
-
-    link "/etc/apache2/proxy-enabled/rtqaproxy.conf" do
-      to "../proxy-available/rtqaproxy.conf"
-      owner "root"
-      group "root"
-      only_if "test -f /etc/apache2/proxy-available/rtqaproxy.conf"
-    end
-
-    link "/etc/apache2/proxy-enabled/rtdemoproxy.conf" do
-      to "../proxy-available/rtdemoproxy.conf"
-      owner "root"
-      group "root"
-      only_if "test -f /etc/apache2/proxy-available/rtdemoproxy.conf"
-    end
+directory "/var/www/html/qa"
+  owner  "root"
+  group  "root"
 end
 
-template "/etc/apache2/mods-available/corelogic-dev-mod_proxy" do
-  source "corelogic-dev-mod_proxy.erb"
+directory "/var/www/html/dev"
+  owner  "root"
+  group  "root"
+end
+
+directory "/var/www/html/vpn"
+  owner  "root"
+  group  "root"
+end
+
+template "/etc/httpd/conf/httpd.conf" do
+  source "httpd.conf.erb"
+  owner "root"
+  group "root"
+  mode "0644"
+  notifies :reload, resources(:service => "httpd")
+end
+
+template "/etc/httpd/conf.d/rtdevproxy.conf" do
+  source "rtdevproxy.conf.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  notifies :reload, resources(:service => "httpd")
+end
+
+template "/etc/httpd/conf.d/rtqaproxy.conf" do
+  source "rtqaproxy.conf.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  notifies :reload, resources(:service => "httpd")
+end
+
+template "/etc/httpd/conf.d/rtdemoproxy.conf" do
+  source "rtdemoproxy.conf.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  notifies :reload, resources(:service => "httpd")
+end
+
+template "/etc/httpd/conf.d/demo-vhost.conf"
+  source "demo-vhost.conf.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  notifies :reload, resources(:service => "httpd")
+end
+
+template "/etc/httpd/conf.d/qa-vhost.conf"
+  source "qa-vhost.conf.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  notifies :reload, resources(:service => "httpd")
+end
+
+template "/etc/httpd/conf.d/dev-vhost.conf"
+  source "dev-vhost.conf.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  notifies :reload, resources(:service => "httpd")
+end
+
+template "/etc/httpd/conf.d/datavision-demo.conf" do
+  source "datavison-demo.conf.erb"
   owner "root"
   group "root"
   mode "0644"
   notifies :reload, resources(:service => "apache2")
 end
 
-template "/etc/apache2/mods-available/corelogic-demo-mod_proxy" do
-  source "corelogic-demo-mod_proxy.erb"
+template "/etc/httpd/conf.d/datavision-qa.conf" do
+  source "datavison-qa.conf.erb"
   owner "root"
   group "root"
   mode "0644"
   notifies :reload, resources(:service => "apache2")
 end
 
-template "/etc/apache2/mods-available/corelogic-qa-mod_proxy" do
-  source "corelogic-qa-mod_proxy.erb"
+template "/etc/httpd/conf.d/corelogic-qa.conf" do
+  source "corelogic-qa.conf.erb"
   owner "root"
   group "root"
   mode "0644"
   notifies :reload, resources(:service => "apache2")
 end
 
-template "/etc/apache2/mods-available/datavision-dev-mod_proxy" do
-  source "datavison-dev-mod_proxy.erb"
+template "/etc/httpd/conf.d/corelogic-demo.conf" do
+  source "corelogic-demo.conf.erb"
   owner "root"
   group "root"
   mode "0644"
   notifies :reload, resources(:service => "apache2")
 end
 
-template "/etc/apache2/mods-available/datavision-demo-mod_proxy" do
-  source "datavison-demo-mod_proxy.erb"
+template "/etc/httpd/conf.d/corelogic-dev.conf" do
+  source "corelogic-dev.conf.erb"
   owner "root"
   group "root"
   mode "0644"
   notifies :reload, resources(:service => "apache2")
 end
 
-template "/etc/apache2/mods-available/datavision-qa-mod_proxy" do
-  source "datavison-qa-mod_proxy.erb"
+template "/etc/httpd/conf.d/vpn.conf" do
+  source "vpn.conf.erb"
   owner "root"
   group "root"
   mode "0644"
-  notifies :reload, resources(:service => "apache2")
-end
-
-server_list = Hash.new
-rt_servers = search(:node, "role:Realtrans-CORE AND chef_environment:QA").each do |server|
-  server_list[server.hostname] = server.ipaddress
-end
-
-template "/etc/apache2/mods-available/vpn-mod_proxy" do
-  source "vpn-mod_proxy.erb"
-  owner "root"
-  group "root"
-  mode "0644"
-  notifies :reload, resources(:service => "apache2")
-end
-
-link "/etc/apache2/mods-enabled/proxy_http.load" do
-  to "../mods-available/proxy_http.load"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/proxy_http.load"
-end
-
-link "/etc/apache2/mods-enabled/proxy.conf" do
-  to "../mods-available/proxy.conf"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/proxy.conf"
-end
-
-link "/etc/apache2/mods-enabled/proxy.load" do
-  to "../mods-available/proxy.load"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/proxy.load"
-end
-
-link "/etc/apache2/mods-enabled/proxy_balancer.load" do
-  to "../mods-available/proxy_balancer.load"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/proxy_balancer.load"
-end
-
-link "/etc/apache2/mods-enabled/proxy_balancer.conf" do
-  to "../mods-available/proxy_balancer.conf"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/proxy_balancer.conf"
-end
-
-link "/etc/apache2/mods-enabled/auth_digest.load" do
-  to "../mods-available/auth_digest.load"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/auth_digest.load"
-end
-
-link "/etc/apache2/mods-enabled/ssl.conf" do
-  to "../mods-available/ssl.conf"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/ssl.conf"
-end
-
-link "/etc/apache2/mods-enabled/ssl.load" do
-  to "../mods-available/ssl.load"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/ssl.load"
-end
-
-link "/etc/apache2/mods-enabled/mod-security.load" do
-  to "../mods-available/mod-security.load"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/mod-security.load"
-end
-
-link "/etc/apache2/mods-enabled/mod-security.conf" do
-  to "../mods-available/mod-security.conf"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/mod-security.conf"
-end
-
-link "/etc/apache2/mods-enabled/unique_id.load" do
-  to "../mods-available/unique_id.load"
-  owner "root"
-  group "root"
-  only_if "test -f /etc/apache2/mods-available/unique_id.load"
+  notifies :reload, resources(:service => "httpd")
 end
 
