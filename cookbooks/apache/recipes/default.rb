90f8c22e6d0046ed0b13b9e53a23adcd7073b349
Bryan Phinney <bryan.phinney@altisource.com>
2012-08-06 14:49:04 -0400

    Trying to define empty array in case there are no workers

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -207,17 +207,17 @@ link "/etc/ssl/private" do
 end
 
 ["Dev", "Intdev", "QA", "Demo"].each do |environ|
+  cenNames = []
   cenNames = search(:node, "role:realtrans-cen AND chef_environment:#{environ}")
   cenNames = cenNames.collect { |vhostName| "#{vhostName}" }.join(" ")
   cenNames = cenNames.gsub!("node[","")
-  cenNames = cenNames.gsub!("]","")
-  cenNames = cenNames.gsub!(".#{node[:domain]}","")
+  cenNames = cenNames.gsub!(".#{node[:domain]}]","")
   cenNames = cenNames.split(" ")
+  venNames = []
   venNames = search(:node, "role:realtrans-ven AND chef_environment:#{environ}")
   venNames = venNames.collect { |vhostName| "#{vhostName}" }.join(" ")
   venNames = venNames.gsub!("node[","")
-  venNames = venNames.gsub!("]","")
-  venNames = venNames.gsub!(".#{node[:domain]}","")
+  venNames = venNames.gsub!(".#{node[:domain]}]","")
   venVenNames = venNames.split(" ")
   template "/etc/httpd/proxy.d/realtrans-#{environ}.proxy.conf" do
     source "realtrans.proxy.conf.erb"
