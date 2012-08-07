632487c39a853ed6f0e1843f65d74646120d18db
Bryan Phinney <bryan.phinney@altisource.com>
2012-08-07 14:48:20 -0400

    Updated to add vhost and proxy for realfoundationapp in Dev and QA.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -222,4 +222,43 @@ end
   end
 end
 
+["Dev", "QA"].each do |environ|
+  rfNames = search(:node, "role:realfoundationapp AND chef_environment:#{environ}")
+  rfNames = rfNames.collect { |vhostName| "#{vhostName}" }.join(" ")
+  rfNames = rfNames.gsub!("node[","")
+  rfNames = rfNames.gsub!(".#{node[:domain]}]","")
+  rfNames = rfNames.split(" ")
+  template "/etc/httpd/proxy.d/rf-#{environ}.proxy.conf" do
+    source "rf.proxy.conf.erb"
+    owner "root"
+    group  "root"
+    mode   "0644"
+    notifies :reload, resources(:service => "httpd")
+    variables(
+      :vhostRfWorkers => rfNames,
+      :environ => "#{environ}"
+    )
+  end
+
+  template "/etc/httpd/conf.d/rf-#{environ}.vhost.conf" do
+    source "rfvhost.conf.erb"
+    owner  "root"
+    group  "root"
+    mode   "0644"
+    notifies :reload, resources(:service => "httpd")
+    case "#{environ}"
+    when "Dev"
+      variables(
+        :vhostName => "#{environ}",
+        :serverName => "rfdev"
+      )
+    when "QA"
+      variables(
+        :vhostName => "#{environ}",
+        :serverName => "rfqa"
+      )
+    end
+end
+
+
 
