01fbaf0c6cc0d19efac7d296c7730d52ecc3cb57
Bryan Phinney <bryan.phinney@altisource.com>
2012-08-10 10:10:07 -0400

    Trying to update realfoundationapp to pull webserver name from databag

--- cookbooks/realfoundationapp/recipes/default.rb
+++ cookbooks/realfoundationapp/recipes/default.rb
@@ -26,15 +26,18 @@ yum_package "#{app_name}" do
   allow_downgrade true
   notifies :restart, resources(:service => "altitomcat")
 end
+
 webHost = data_bag_item("apache-server", "webhost")
-environ = node[:chef_environment]
+env = node[:chef_environment]
 template "/opt/tomcat/conf/realfoundationapp.properties" do
   source "realfoundationapp.properties.erb"
   group 'tomcat'
   owner 'tomcat'
   mode '0644'
   notifies :restart, resources(:service => "altitomcat")
-  variables( :web_server_hostname => webHost["rf#{environ}"] )
+  variables( 
+    :webHostname => webHost["rf#{env}"]
+  )
 end
 
 template "/opt/tomcat/conf/Catalina/localhost/realfoundationapp.xml" do
