046dd7057b0c8134f56844bf3ea55513534fd89f
Bryan Phinney <bryan.phinney@altisource.com>
2012-08-15 12:26:42 -0400

    Updated to install appdynamics-agent package

--- cookbooks/appdynamics/recipes/default.rb
+++ cookbooks/appdynamics/recipes/default.rb
@@ -7,16 +7,7 @@
 # All rights reserved - Do Not Redistribute
 #
 
-directory "/opt/appdynamic-agent" do
-  owner "root"
-  group "root"
-end
-
-execute "appdynamicdownload" do
-  user "root"
-  cwd  "/tmp"
-  command "wget -O /tmp/AppServerAgent.zip http://10.0.0.20/yum/common/AppServerAgent.zip; cd /opt/appdynamic-agent; unzip /tmp/AppServerAgent.zip; rm -f /tmp/AppServerAgent.zip"
-  creates "/opt/appdynamic-agent/javaagent.jar"
-  action :run
+package "appdynamic-agent" do
+  :upgrade
 end
 
