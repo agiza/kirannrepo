647cd06c409ab0bf30f2e9315e254efe9b9ff3f4
Bryan Phinney <bryan.phinney@altisource.com>
2012-08-20 12:45:14 -0400

    Adding recipe for hubzu application

--- cookbooks/hubzu/metadata.rb
+++ cookbooks/hubzu/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Altisource"
 maintainer_email "bryan.phinney@altisource.com"
 license          "All rights reserved"
-description      "Installs/Configures realfoundationapp"
+description      "Installs/Configures hubzu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
