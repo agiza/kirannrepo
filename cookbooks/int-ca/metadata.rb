29e2d24f9e63725c30237184ca235e078b29bd5a
Bryan Phinney <bryan.phinney@altisource.com>
2012-09-11 07:14:25 -0400

    Updating to add new int-ca cookbook

---
+++ cookbooks/int-ca/metadata.rb
@@ -0,0 +1,7 @@
+maintainer       "Altisource"
+maintainer_email "bryan.phinney@altisource.com"
+license          "All rights reserved"
+description      "Installs/Configures int-ca"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          "0.0.1"
+depends		 "altitomcat"
---
+++ cookbooks/int-ca/recipes/default.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: int-ca
+# Recipe:: default
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+app_name = "int-ca"
+app_version = node[:intca_version]
+
+include_recipe "altitomcat"
+
+service "altitomcat" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+yum_package "#{app_name}" do
+  version "#{app_version}"
+  case node.chef_environment
+  when "Dev","Intdev"
+    action :nothing
+  else
+    action :install
+  end
+  flush_cache [ :before ]
+  allow_downgrade true
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+template "/opt/tomcat/conf/int-collateralanalytics.properties" do
+  source "int-collateralanalytics.properties.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
+
