5115710c1609078e5dc480d818a6966331f18a6d
Bryan Phinney <bryan.phinney@altisource.com>
2012-09-18 09:22:10 -0400

    Updated to try to grab first in multiple entries

--- cookbooks/realtrans-rp/recipes/default.rb
+++ cookbooks/realtrans-rp/recipes/default.rb
@@ -33,7 +33,7 @@ rdochost = {}
 search(:node, "role:realdoc AND chef_environment:#{node.chef_environment}") do |n|
   rdochost[n.hostname] = {}
 end
-#rdochostname = rdochost[0]
+rdochostname = rdochost[0]
 webHost = data_bag_item("apache-server", "webhost")
 template "/opt/tomcat/conf/realtrans-rp.properties" do
   source "realtrans-rp.properties.erb"
@@ -43,7 +43,7 @@ template "/opt/tomcat/conf/realtrans-rp.properties" do
   notifies :restart, resources(:service => "altitomcat")
   variables(
     :webHostname => webHost["rt#{node.chef_environment}"],
-    :realdoc_hostname => "#{rdochost}"
+    :realdoc_hostname => "#{rdochostname}"
   )
 end
 
