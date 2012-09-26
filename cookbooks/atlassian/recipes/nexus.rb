5aa672f9eff8f0b7b0462c85c3693dfb150ed849
Bryan Phinney <bryan.phinney@altisource.com>
2012-09-26 15:03:46 -0400

    Updated to combine all atlassian recipes into a single cookbook

---
+++ cookbooks/atlassian/recipes/nexus.rb
@@ -0,0 +1,39 @@
+#
+# Cookbook Name:: atlassian
+# Recipe:: nexus
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+
+template "/root/mount-storage.sh" do
+  source "mount-storage-nexus.sh.erb"
+  owner  "root"
+  group  "root"
+  mode   "0755"
+end
+
+execute "mountopt" do
+  command "/root/mount-storage.sh"
+  creates "/storage/lost+found"
+  action :run
+end
+
+template "/etc/init.d/nexus" do
+  source "nexus-init.erb"
+  owner  "root"
+  group  "root"
+  mode   "0755"
+end
+
+service "nexus" do
+  supports :start => true, :stop => true, :restart => true, :status => true
+  action [:enable, :start]
+end
+
+cron "backups" do
+  minute "0"
+  user   "root"
+  command "rsync -av --delete /storage/ /backups/"
+end
