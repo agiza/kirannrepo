850d884653329bebfcc3d897d4d5bf0a6ddedbdd
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-02 09:22:38 -0400

    Updating to move hostname recipe into infrastructure cookbook

---
+++ cookbooks/infrastructure/recipes/hostname.rb
@@ -0,0 +1,41 @@
+#
+# Cookbook Name:: infrastructure
+# Recipe:: hostname
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+
+host_name=node[:fqdn]
+execute "hostname" do
+  case node[:platform]
+  when "centos", "redhat", "fedora", "suse"
+    command "hostname #{host_name} "
+    action :run
+  when "debian", "ubuntu"
+    command "sudo hostname #{host_name} "
+    action :run
+  end
+end
+
+template "/etc/init.d/host_command" do
+  source "host_command.erb"
+  mode "0755"
+  owner "root"
+  group "root"
+end
+
+case node[:platform]
+when "centos", "redhat", "fedora", "suse"
+  execute "chkconfig" do
+    command " chkconfig host_command on"
+    action :run
+  end
+when "debian", "ubuntu"
+  execute "update-rc.d" do
+    command "update-rc.d host_command defaults; update-rc.d host_command enable"
+    action :run
+  end
+end
+
