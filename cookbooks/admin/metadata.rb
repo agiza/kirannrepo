83997f18720aa8a9c78a902df5061fd9d4c8ad74
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-02 09:21:19 -0400

    Updated to move recipes into infrastructure cookbook

--- cookbooks/admin/metadata.rb
+++
@@ -1,6 +0,0 @@
-maintainer       "Altisource"
-maintainer_email "bryan.phinney@altisource.com"
-license          "All rights reserved"
-description      "Installs/Configures admin"
-long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
