44bb82c614ba4800d27bf01a8e3d76cae31fff7c
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-08 14:55:27 -0400

    Updated to create realtrans-central recipe for proper distribution of recipes

--- cookbooks/realtrans/recipes/default.rb
+++ cookbooks/realtrans/recipes/default.rb
@@ -4,46 +4,5 @@
 #
 
 #include_recipe "java"
-app_name = "realtrans-central"
-app_version = node[:realtranscentral_version]
 
-include_recipe "altisource::altitomcat"
 
-service "altitomcat" do
-  supports :stop => true, :start => true, :restart => true, :reload => true
-  action :nothing
-end
-
-yum_package "#{app_name}" do
-  version "#{app_version}"
-  case node.chef_environment
-  when "Dev","Intdev"
-    action :nothing
-  else
-    action :install
-  end
-  flush_cache [ :before ]
-  allow_downgrade true
-  notifies :restart, resources(:service => "altitomcat")
-end
-
-rtcenhost = {}
-search(:node, "role:realtrans-cen AND chef_environment:#{node.chef_environment}") do |n|
-  rtcenhost[n.hostname] = {}
-end
-template "/opt/tomcat/conf/#{app_name}.properties" do
-  source "#{app_name}.properties.erb"
-  group 'tomcat'
-  owner 'tomcat'
-  mode '0644'
-  variables( :rt_cen_host => "#{rtcenhost}")
-  notifies :restart, resources(:service => "altitomcat")
-end
-
-template "/opt/tomcat/conf/Catalina/localhost/#{app_name}.xml" do
-  source "#{app_name}.xml.erb"
-  group 'tomcat'
-  owner 'tomcat'
-  mode '0644'
-  notifies :restart, resources(:service => "altitomcat")
-end
