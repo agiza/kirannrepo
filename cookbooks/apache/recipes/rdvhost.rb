b6912c5ab4b6b6a134bb1388ed0cc7f4007a3f0b
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-10 09:06:08 -0400

    Updated to add unless conditional to all vhost recipes

--- cookbooks/apache/recipes/rdvhost.rb
+++ cookbooks/apache/recipes/rdvhost.rb
@@ -13,51 +13,53 @@ search(:node, "role:realdoc") do |n|
   rdenvirons[n.chef_environment] = {}
 end
 
-# Convert the hash list of environments into a string, unique values, then split
-rdenvirons = rdenvirons.collect { |rdenviron| "#{rdenviron}" }.join(" ").split.uniq.join(" ").split(" ")
+unless rdenvirons == "nil"
+  # Convert the hash list of environments into a string, unique values, then split
+  rdenvirons = rdenvirons.collect { |rdenviron| "#{rdenviron}" }.join(" ").split.uniq.join(" ").split(" ")
 
-# Databag item for webserver hostname
-webName = data_bag_item("apache-server", "webhost")
-sslflag = webName['sslflag']
-case "#{sslflag}"
-when "true"
-  ssl = ".ssl"
-when "false", "nil"
-  ssl = ""
-end
-serveripallow = webName['serveripallow'].split("|")
+  # Databag item for webserver hostname
+  webName = data_bag_item("apache-server", "webhost")
+  sslflag = webName['sslflag']
+  case "#{sslflag}"
+  when "true"
+    ssl = ".ssl"
+  when "false", "nil"
+    ssl = ""
+  end
+  serveripallow = webName['serveripallow'].split("|")
 
-# Loop through list of environments to build workers and pass to the vhost/proxy templates
-rdenvirons.each do |environ|
-  rdNames = search(:node, "role:realdoc AND chef_environment:#{environ}")
-  rdNames = rdNames.collect { |vhostName| "#{vhostName}" }.join(" ").gsub!("node[","").gsub!(".#{node[:domain]}]","").split(" ")
-  template "/etc/httpd/proxy.d/rd-#{environ}.proxy.conf" do
-    source "rd.proxy.conf.erb"
-    owner "root"
-    group  "root"
-    mode   "0644"
-    notifies :reload, resources(:service => "httpd")
-    variables(
-      :vhostRdWorkers => rdNames,
-      :environ => "#{environ}",
-      :serveripallow => serveripallow
-    )
-  end
-  template "/etc/httpd/conf.d/rd-#{environ}.vhost.conf" do
-    source "rdvhost#{ssl}.conf.erb"
-    owner  "root"
-    group  "root"
-    mode   "0644"
-    notifies :reload, resources(:service => "httpd")
-    variables(
-      :vhostName => "#{environ}",
-      :testvhostName => webName["rd#{environ}"],
-      :serverName => webName["rd#{environ}"]
-    )
-  end
-   directory "/var/www/html/#{environ}" do
-    owner "root"
-    group "root"
+  # Loop through list of environments to build workers and pass to the vhost/proxy templates
+  rdenvirons.each do |environ|
+    rdNames = search(:node, "role:realdoc AND chef_environment:#{environ}")
+    rdNames = rdNames.collect { |vhostName| "#{vhostName}" }.join(" ").gsub!("node[","").gsub!(".#{node[:domain]}]","").split(" ")
+    template "/etc/httpd/proxy.d/rd-#{environ}.proxy.conf" do
+      source "rd.proxy.conf.erb"
+      owner "root"
+      group  "root"
+      mode   "0644"
+      notifies :reload, resources(:service => "httpd")
+      variables(
+        :vhostRdWorkers => rdNames,
+        :environ => "#{environ}",
+        :serveripallow => serveripallow
+      )
+    end
+    template "/etc/httpd/conf.d/rd-#{environ}.vhost.conf" do
+      source "rdvhost#{ssl}.conf.erb"
+      owner  "root"
+      group  "root"
+      mode   "0644"
+      notifies :reload, resources(:service => "httpd")
+      variables(
+        :vhostName => "#{environ}",
+        :testvhostName => webName["rd#{environ}"],
+        :serverName => webName["rd#{environ}"]
+      )
+    end
+    directory "/var/www/html/#{environ}" do
+      owner "root"
+      group "root"
+    end
   end
 end
 
--- cookbooks/apache/recipes/rfvhost.rb
+++ cookbooks/apache/recipes/rfvhost.rb
@@ -13,54 +13,56 @@ search(:node, "role:realfoundationapp") do |n|
   rfenvirons[n.chef_environment] = {}
 end
 
-# Convert the hash list of environments into a string, unique values, then split
-rfenvirons = rfenvirons.collect { |rfenviron| "#{rfenviron}" }.join(" ")
-rfenvirons = rfenvirons.split.uniq.join(" ")
-rfenvirons = rfenvirons.split(" ")
+unless rfenvirons == "nil"
+  # Convert the hash list of environments into a string, unique values, then split
+  rfenvirons = rfenvirons.collect { |rfenviron| "#{rfenviron}" }.join(" ")
+  rfenvirons = rfenvirons.split.uniq.join(" ")
+  rfenvirons = rfenvirons.split(" ")
 
-# Databag item for webserver hostname
-webName = data_bag_item("apache-server", "webhost")
-sslflag = webName['sslflag']
-case "#{sslflag}"
-when "true"
-  ssl = ".ssl"
-when "false", "nil"
-  ssl = ""
-end
-serveripallow = webName['serveripallow'].split("|")
+  # Databag item for webserver hostname
+  webName = data_bag_item("apache-server", "webhost")
+  sslflag = webName['sslflag']
+  case "#{sslflag}"
+  when "true"
+    ssl = ".ssl"
+  when "false", "nil"
+    ssl = ""
+  end
+  serveripallow = webName['serveripallow'].split("|")
 
-# Loop through list of environments to build workers and pass to the vhost/proxy templates
-rfenvirons.each do |environ|
-  rfNames = search(:node, "role:realfoundationapp AND chef_environment:#{environ}")
-  rfNames = rfNames.collect { |vhostName| "#{vhostName}" }.join(" ")
-  rfNames = rfNames.gsub!("node[","").gsub!(".#{node[:domain]}]","").split(" ")
-  template "/etc/httpd/proxy.d/rf-#{environ}.proxy.conf" do
-    source "rf.proxy.conf.erb"
-    owner "root"
-    group  "root"
-    mode   "0644"
-    notifies :reload, resources(:service => "httpd")
-    variables(
-      :vhostRfWorkers => rfNames,
-      :environ => "#{environ}",
-      :serveripallow => serveripallow
-    )
-  end
-  template "/etc/httpd/conf.d/rf-#{environ}.vhost.conf" do
-    source "rfvhost#{ssl}.conf.erb"
-    owner  "root"
-    group  "root"
-    mode   "0644"
-    notifies :reload, resources(:service => "httpd")
-    variables(
-      :vhostName => "#{environ}",
-      :testvhostName => webName["rf#{environ}"],
-      :serverName => webName["rf#{environ}"]
-    )
-  end
-   directory "/var/www/html/#{environ}" do
-    owner "root"
-    group "root"
+  # Loop through list of environments to build workers and pass to the vhost/proxy templates
+  rfenvirons.each do |environ|
+    rfNames = search(:node, "role:realfoundationapp AND chef_environment:#{environ}")
+    rfNames = rfNames.collect { |vhostName| "#{vhostName}" }.join(" ")
+    rfNames = rfNames.gsub!("node[","").gsub!(".#{node[:domain]}]","").split(" ")
+    template "/etc/httpd/proxy.d/rf-#{environ}.proxy.conf" do
+      source "rf.proxy.conf.erb"
+      owner "root"
+      group  "root"
+      mode   "0644"
+      notifies :reload, resources(:service => "httpd")
+      variables(
+        :vhostRfWorkers => rfNames,
+        :environ => "#{environ}",
+        :serveripallow => serveripallow
+      )
+    end
+    template "/etc/httpd/conf.d/rf-#{environ}.vhost.conf" do
+      source "rfvhost#{ssl}.conf.erb"
+      owner  "root"
+      group  "root"
+      mode   "0644"
+      notifies :reload, resources(:service => "httpd")
+      variables(
+        :vhostName => "#{environ}",
+        :testvhostName => webName["rf#{environ}"],
+        :serverName => webName["rf#{environ}"]
+      )
+    end
+    directory "/var/www/html/#{environ}" do
+      owner "root"
+      group "root"
+    end
   end
 end
 
