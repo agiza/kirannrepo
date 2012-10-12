dc6eaa512a72064c427b72571f66cd987c0dc251
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-12 09:10:38 -0400

    Updated to move the host detection function back into the recipes

--- cookbooks/realtrans/recipes/realtrans-fp.rb
+++ cookbooks/realtrans/recipes/realtrans-fp.rb
@@ -10,7 +10,23 @@ app_name = "realtrans-fp"
 app_version = node[:realtransfp_version]
 
 include_recipe "altisource::altitomcat"
-include_recipe "realtrans::default"
+rdochost = {}
+case node.chef_environment
+when "Intdev"
+  search(:node, "role:realdoc AND chef_environment:Dev") do |n|
+  rdochost[n.hostname] = {}
+  end
+else
+  search(:node, "role:realdoc AND chef_environment:#{node.chef_environment}") do |n|
+  rdochost[n.hostname] = {}
+  end
+end
+rdochost = rdochost.first
+rtcenhost = {}
+search(:node, "role:realtrans-cen AND chef_environment:#{node.chef_environment}") do |n|
+  rtcenhost[n.hostname] = {}
+end
+rtcenhost = rtcenhost.first
 
 service "altitomcat" do
   supports :stop => true, :start => true, :restart => true, :reload => true
