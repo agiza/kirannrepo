748f003b2cddb66bb951fd7474dfa4a70759cb4a
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-12 13:03:38 -0400

    Updated to make elasticsearch more descriptive with recipes

---
+++ cookbooks/elasticsearch/recipes/server.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: elasticsearch
+# Recipe:: server
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package "elasticsearch" do
+  action :upgrade
+end
+
+directory "/opt/elasticsearch/logs" do
+  owner  "elasticsearch"
+  group  "elasticsearch"
+  mode   "0644"
+  action :create
+end
+
+directory "/opt/elasticsearch/data" do
+  owner  "elasticsearch"
+  group  "elasticsearch"
+  mode   "0644"
+  action :create
+end
+
+service "elasticsearch" do
+  supports :start => true, :stop => true, :restart => true, :reload => true
+  action :nothing
+end
+
+template "/etc/elasticsearch/elasticsearch.yml" do
+  source "elasticsearch.yml.erb"
+  owner  "elasticsearch"
+  group  "elasticsearch"
+  notifies :restart, resources(:service => "elasticsearch")
+end
+
+template "/etc/sysconfig/sysconfig-elasticsearch" do
+  source "sysconfig-elasticsearch.erb"
+  owner  "root"
+  group  "root"
+  notifies :restart, resources(:service => "elasticsearch")
+end
+
+service "elasticsearch" do
+  action [:enable, :start]
+end
+
