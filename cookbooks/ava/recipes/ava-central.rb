2dded318b1fa58531bab8727d5921a63e0ee1ee1
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-12 09:20:15 -0400

    Updated to pull first item of potential multiple hosts for realdoc and ava central host

--- cookbooks/ava/recipes/ava-central.rb
+++ cookbooks/ava/recipes/ava-central.rb
@@ -9,6 +9,24 @@ app_version = node[:avacentral_version]
 
 include_recipe "altisource::altitomcat"
 
+rdochost = {}
+case node.chef_environment
+when "Intdev"
+  search(:node, "role:realdoc AND chef_environment:Dev") do |n|
+  rdochost[n.hostname] = {}
+  end
+else
+  search(:node, "role:realdoc AND chef_environment:#{node.chef_environment}") do |n|
+  rdochost[n.hostname] = {}
+  end
+end
+rdochost = rdochost.first
+avacenhost = {}
+search(:node, "role:ava-cen AND chef_environment:#{node.chef_environment}") do |n|
+  avacenhost[n.hostname] = {}
+end
+avacenhost = avacenhost.first
+
 service "altitomcat" do
   supports :stop => true, :start => true, :restart => true, :reload => true
   action :nothing
@@ -27,10 +45,6 @@ yum_package "#{app_name}" do
   notifies :restart, resources(:service => "altitomcat")
 end
 
-avacenhost = {}
-search(:node, "role:ava-cen AND chef_environment:#{node.chef_environment}") do |n|
-  avacenhost[n.hostname] = {}
-end
 template "/opt/tomcat/conf/#{app_name}.properties" do
   source "#{app_name}.properties.erb"
   group 'tomcat'
--- cookbooks/ava/recipes/ava-fp.rb
+++ cookbooks/ava/recipes/ava-fp.rb
@@ -10,6 +10,24 @@ app_name = "ava-fp"
 app_version = node[:avafp_version]
 
 include_recipe "altisource::altitomcat"
+rdochost = {}
+case node.chef_environment
+when "Intdev"
+  search(:node, "role:realdoc AND chef_environment:Dev") do |n|
+  rdochost[n.hostname] = {}
+  end
+else
+  search(:node, "role:realdoc AND chef_environment:#{node.chef_environment}") do |n|
+  rdochost[n.hostname] = {}
+  end
+end
+rdochost = rdochost.first
+avacenhost = {}
+search(:node, "role:ava-cen AND chef_environment:#{node.chef_environment}") do |n|
+  avacenhost[n.hostname] = {}
+end
+avacenhost = avacenhost.first
+
 
 service "altitomcat" do
   supports :stop => true, :start => true, :restart => true, :reload => true
@@ -29,14 +47,6 @@ yum_package "#{app_name}" do
   notifies :restart, resources(:service => "altitomcat")
 end
 
-rdochost = {}
-search(:node, "role:realdoc AND chef_environment:#{node.chef_environment}") do |n|
-  rdochost[n.hostname] = {}
-end
-avacenhost = {}
-search(:node, "role:ava-cen AND chef_environment:#{node.chef_environment}") do |n|
-  avacenhost[n.hostname] = {}
-end
 webHost = data_bag_item("apache-server", "webhost")
 template "/opt/tomcat/conf/#{app_name}.properties" do
   source "#{app_name}.properties.erb"
