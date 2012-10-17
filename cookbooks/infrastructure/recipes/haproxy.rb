3daffaf6ba68f4ee1be9bd66ceb8c265e6789b07
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-17 12:53:00 -0400

    Updated haproxy to not create rabbit entries.

--- cookbooks/infrastructure/recipes/haproxy.rb
+++ cookbooks/infrastructure/recipes/haproxy.rb
@@ -7,17 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 app_name = "haproxy"
-if node.attribute?('amqpproxy')
-  amqphost = node[:amqpproxy]
-  amqpport = node[:amqpport]
-else
-  amqphost = {}
-  search(:node, "role:rabbitserver") do |n|
-    amqphost[n.ipaddress] = {}
-  end
-  amqphost = amqphost.first
-  amqpport = "5672"
-end
 
 package "haproxy" do
   action :upgrade
@@ -26,24 +15,6 @@ end
 service "haproxy" do
   supports :stop => true, :start => true, :restart => true, :reload => true
   action :nothing
-end
-
-clusternodes = {}
-search(:node, "role:rabbitserver") do |n|
-  clusternodes[n.hostname] = {}
-end
-
-
-template "/etc/haproxy/haproxy.cfg" do
-  source "haproxy.cfg.erb"
-  group "haproxy"
-  owner "haproxy"
-  mode "0644"
-  variables(
-    :clusternodes => clusternodes,
-    :amqpport => "#{amqpport}"
-  )
-  notifies :restart, resources(:service => "haproxy")
 end
 
 service "haproxy" do
