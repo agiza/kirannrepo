384da173b5a84124f7747a482ae2c2ab6ca1b946
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-17 08:16:59 -0400

    Updated to add rabbitproxy recipe to differentiate haproxy for rabbit from others

---
+++ cookbooks/infrastructure/recipes/rabbitproxy.rb
@@ -0,0 +1,52 @@
+#
+# Cookbook Name:: infrastructure
+# Recipe:: rabbitproxy
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+app_name = "haproxy"
+if node.attribute?('amqpproxy')
+  amqphost = node[:amqpproxy]
+  amqpport = node[:amqpport]
+else
+  amqphost = {}
+  search(:node, "role:rabbitserver") do |n|
+    amqphost[n.ipaddress] = {}
+  end
+  amqphost = amqphost.first
+  amqpport = "5672"
+end
+
+package "haproxy" do
+  action :upgrade
+end
+
+service "haproxy" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+clusternodes = {}
+search(:node, "role:rabbitserver") do |n|
+  clusternodes[n.ipaddress] = {}
+end
+
+
+template "/etc/haproxy/haproxy.cfg" do
+  source "rabbitproxy.cfg.erb"
+  group "haproxy"
+  owner "haproxy"
+  mode "0644"
+  variables(
+    :clusternodes => clusternodes,
+    :amqpport => "#{amqpport}"
+  )
+  notifies :restart, resources(:service => "haproxy")
+end
+
+service "haproxy" do
+  action [:enable, :start]
+end
+
