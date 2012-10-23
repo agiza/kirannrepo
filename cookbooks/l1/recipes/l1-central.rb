c91de5e9abc8f2a078fa5c7af705def52f3c3208
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-23 13:50:38 -0400

    Updated to try to include the common code from the default recipe.

--- cookbooks/l1/recipes/l1-central.rb
+++ cookbooks/l1/recipes/l1-central.rb
@@ -8,36 +8,7 @@ app_name = "l1-central"
 app_version = node[:l1central_version]
 
 include_recipe "altisource::altitomcat"
-
-if node.attribute?('realdocproxy')
-  rdochost = node[:realdocproxy]
-else
-  rdochost = {}
-  search(:node, "role:realdoc AND chef_environment:#{node.chef_environment}") do |n|
-    rdochost[n.ipaddress] = {}
-  end
-  rdochost = rdochost.first
-end
-if node.attribute?('l1cenproxy')
-  l1cenhost = node[:l1cenproxy]
-else
-  l1cenhost = {}
-  search(:node, "role:l1-cen AND chef_environment:#{node.chef_environment}") do |n|
-    l1cenhost[n.ipaddress] = {}
-  end
-  l1cenhost = l1cenhost.first
-end
-if node.attribute?('amqpproxy')
-  amqphost = node[:amqpproxy]
-  amqpport = node[:amqpport]
-else
-  amqphost = {}
-  search(:node, "role:rabbitserver") do |n|
-    amqphost[n.ipaddress] = {}
-  end
-  amqphost = amqphost.first
-  amqpport = "5672"
-end
+include_recipe "l1::default"
 
 service "altitomcat" do
   supports :stop => true, :start => true, :restart => true, :reload => true
@@ -56,9 +27,9 @@ yum_package "#{app_name}" do
   notifies :restart, resources(:service => "altitomcat")
 end
 
-l1rabbit = data_bag_item("rabbitmq", "realtrans")
-l1rabbit = l1rabbit['user'].split("|")
-melissadata = data_bag_item("integration", "melissadata")
+#l1rabbit = data_bag_item("rabbitmq", "realtrans")
+#l1rabbit = l1rabbit['user'].split("|")
+#melissadata = data_bag_item("integration", "melissadata")
 template "/opt/tomcat/conf/#{app_name}.properties" do
   source "#{app_name}.properties.erb"
   group 'tomcat'
