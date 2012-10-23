2b3a6dd29ef8266f68fdc50d23b940fcc0008ad9
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-23 09:28:19 -0400

    Updated to move search into the recipe.  Need to define these in a more seamless way.

--- cookbooks/apache/recipes/default.rb
+++ cookbooks/apache/recipes/default.rb
@@ -114,6 +114,7 @@ template "/etc/pki/tls/private/#{servername[0]}.key" do
   notifies :reload, resources(:service => "httpd")
 end
 
+corelogicdev = search(:node, "role:l1-integration AND chef_environment:Dev")
 sitesinclude = data_bag_item("apache-server", "webhost")
 sitesinclude = sitesinclude['serversites'].split("|")
 sitesinclude.each do |site|
@@ -122,6 +123,7 @@ sitesinclude.each do |site|
     owner  "root"
     group  "root"
     mode   "0644"
+    variables( :corelogicdev => corelogicdev )
     notifies :reload, resources(:service => "httpd")
   end
 end
