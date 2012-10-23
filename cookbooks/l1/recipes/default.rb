2123fcec3b455f35bac87ea0d8eac54a0eb9c5f8
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-23 13:52:24 -0400

    Updated to include common code that is needed.

--- cookbooks/l1/recipes/default.rb
+++ cookbooks/l1/recipes/default.rb
@@ -5,4 +5,38 @@
 
 #include_recipe "java"
 
+if node.attribute?('realdocproxy')
+  rdochost = node[:realdocproxy]
+else
+  rdochost = {}
+  search(:node, "role:realdoc AND chef_environment:#{node.chef_environment}") do |n|
+    rdochost[n.ipaddress] = {}
+  end
+  rdochost = rdochost.first
+end
+if node.attribute?('l1cenproxy')
+  l1cenhost = node[:l1cenproxy]
+else
+  l1cenhost = {}
+  search(:node, "role:l1-cen AND chef_environment:#{node.chef_environment}") do |n|
+    l1cenhost[n.ipaddress] = {}
+  end
+  l1cenhost = l1cenhost.first
+end
+if node.attribute?('amqpproxy')
+  amqphost = node[:amqpproxy]
+  amqpport = node[:amqpport]
+else
+  amqphost = {}
+  search(:node, "role:rabbitserver") do |n|
+    amqphost[n.ipaddress] = {}
+  end
+  amqphost = amqphost.first
+  amqpport = "5672"
+end
+
+l1rabbit = data_bag_item("rabbitmq", "realtrans")
+l1rabbit = l1rabbit['user'].split("|")
+melissadata = data_bag_item("integration", "melissadata")
+
 
