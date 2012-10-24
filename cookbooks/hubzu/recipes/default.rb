8eff531295a72754ddc98d4e62a7ee92f51a56c1
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-24 11:21:05 -0400

    Updated hubzu to get its own recipe as well as bring it up to date with the data bag elements.

--- cookbooks/hubzu/recipes/default.rb
+++ cookbooks/hubzu/recipes/default.rb
@@ -6,44 +6,4 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-app_name = "hubzu"
-app_version = node[:hubzu_version]
-
-service "altitomcat" do
-  supports :stop => true, :start => true, :restart => true, :reload => true
-  action :nothing
-end
-
-yum_package "#{app_name}" do
-  version "#{app_version}"
-  case node.chef_environment
-  when "Dev","Intdev"
-    action :nothing
-  else
-    action :install
-  end
-  flush_cache [ :before ]
-  allow_downgrade true
-  notifies :restart, resources(:service => "altitomcat")
-end
-
-webHost = data_bag_item("apache-server", "webhost")
-template "/opt/tomcat/conf/hubzu.properties" do
-  source "hubzu.properties.erb"
-  group 'tomcat'
-  owner 'tomcat'
-  mode '0644'
-  notifies :restart, resources(:service => "altitomcat")
-  variables( 
-    :webHostname => webHost["hz#{node.chef_environment}"]
-  )
-end
-
-template "/opt/tomcat/conf/Catalina/localhost/hubzu.xml" do
-  source "hubzu.xml.erb"
-  group 'tomcat'
-  owner 'tomcat'
-  mode '0644'
-  notifies :restart, resources(:service => "altitomcat")
-end
 
---
+++ cookbooks/hubzu/recipes/hubzu.rb
@@ -0,0 +1,53 @@
+#
+# Cookbook Name:: hubzu
+# Recipe:: hubzu
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+app_name = "hubzu"
+app_version = node[:hubzu_version]
+
+service "altitomcat" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+yum_package "#{app_name}" do
+  version "#{app_version}"
+  case node.chef_environment
+  when "Dev","Intdev"
+    action :nothing
+  else
+    action :install
+  end
+  flush_cache [ :before ]
+  allow_downgrade true
+  notifies :restart, resources(:service => "altitomcat")
+end
+
+webHost = data_bag_item("apache-server", "webhost")
+melissadata = data_bag_item("integration", "melissadata")
+mailserver = data_bag_item("integration", "mail")
+template "/opt/tomcat/conf/hubzu.properties" do
+  source "hubzu.properties.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+  variables( 
+    :webHostname => webHost["hz#{node.chef_environment}"],
+    :mailserver => mailserver,
+    :melissadata => melissadata['melissadata']
+  )
+end
+
+template "/opt/tomcat/conf/Catalina/localhost/hubzu.xml" do
+  source "hubzu.xml.erb"
+  group 'tomcat'
+  owner 'tomcat'
+  mode '0644'
+  notifies :restart, resources(:service => "altitomcat")
+end
+
