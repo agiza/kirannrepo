9306e614e8d100d37df71cf9020dd548f5608a82
Bryan Phinney <bryan.phinney@altisource.com>
2012-10-25 15:14:54 -0400

    Fixed an error.

--- cookbooks/apache/recipes/l1vhost.rb
+++ cookbooks/apache/recipes/l1vhost.rb
@@ -16,7 +16,7 @@ if l1cenenvirons.nil? || l1cenenvirons.empty?
   Chef::Log.info("No services returned from search.")
 else
   # Databag item for webserver hostname
-  #webName = data_bag_item("apache-server", "webhost")
+  webName = data_bag_item("apache-server", "webhost")
   if node.attribute?('ssl_force')
     ssl = ".ssl"
   else 
