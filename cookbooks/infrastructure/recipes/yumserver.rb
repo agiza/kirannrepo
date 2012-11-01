38dd6e8433410e870d189edc580cda25c2c80b3e
Bryan Phinney <bryan.phinney@altisource.com>
2012-11-01 12:50:08 -0400

    Updated to add a recipe to setup a yum server.

---
+++ cookbooks/infrastructure/recipes/yumserver.rb
@@ -0,0 +1,63 @@
+#
+# Cookbook Name:: infrastructure
+# Recipe:: yumserver
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+
+yum_package "createrepo" do
+  action :upgrade
+end
+
+yum_package "httpd" do
+  action :upgrade
+end
+
+directory "/var/www/html/yum-repo" do
+  owner "root"
+  group "root"
+end
+
+directory "/var/www/html/yum-repo/release" do
+  owner  "root"
+  group  "root"
+end
+
+directory "/var/www/html/yum-repo/common" do
+  owner "root"
+  group "root"
+end
+
+directory "/var/www/html/yum-repo/testing" do
+  owner  "root"
+  group  "root"
+end
+
+service "httpd" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+template "/usr/local/bin/yum-update" do
+  source "yum-update.erb"
+  owner  "root"
+  group  "root"
+  mode   "0755"
+end
+
+template "/etc/httpd/conf.d/yum.conf" do
+  source "yum.conf.erb"
+  owner  "root"
+  group  "root"
+  mode   "0755"
+  notifies :restart, resources(:service => "httpd")
+end
+
+cron "yum-update" do
+  minute "0,10,20,30,40,50"
+  user  "root"
+  command "/usr/local/bin/yum-update > /dev/null 2>&1"
+end
+
