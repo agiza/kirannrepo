3da3bcf89c7ca92d30374d674c991422fb2e5d58
Bryan Phinney <bryan.phinney@altisource.com>
2012-11-13 09:11:23 -0500

    Updating metadata to include dependency for altisource cookbook.

--- cookbooks/rabbitmq/metadata.rb
+++ cookbooks/rabbitmq/metadata.rb
@@ -5,3 +5,4 @@ description      "Installs/Configures rabbitmq"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
 depends          "infrastructure"
+depends		 "altisource"
