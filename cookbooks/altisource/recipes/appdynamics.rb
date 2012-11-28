30dbe26aac63dd492966cdc023e086ed3a5b3f9f
Bryan Phinney <bryan.phinney@altisource.com>
2012-11-28 18:33:24 +0000

    Updated to add host and port for controller file

--- cookbooks/altisource/recipes/appdynamics.rb
+++ cookbooks/altisource/recipes/appdynamics.rb
@@ -11,10 +11,20 @@ package "appdynamic-agent" do
   action :upgrade
 end
 
+appdynhost = {}
+search(:node, "role:appdynamics-server") do |n|
+  appdynhost[n.ipaddress] = {}
+end
+appdynhost = appdynhost.first
+
 template "/opt/appdynamic-agent/conf/controller-info.xml" do
   source "controller-info.xml.erb"
   owner  "tomcat"
   group  "tomcat"
   mode   "0644"
+  variables(
+    :appdynhost => appdynhost,
+    :appdynport => "8090"
+  )
 end
 
