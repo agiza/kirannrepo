1e4ce62ce58e1cafd820745cc918ee05a926c7b5
Bryan Phinney <bryan.phinney@altisource.com>
2012-11-30 11:29:10 -0500

    Updated to fix the list.

--- cookbooks/rabbitmq/recipes/rabbitmqserver.rb
+++ cookbooks/rabbitmq/recipes/rabbitmqserver.rb
@@ -57,7 +57,7 @@ rabbitapps.each do |application_name|
     vhost_names << appvhosts
   end
 end
-vhost_names = vhost_names.collect { |vhost| "#{vhost}" }.join("/").split("/").sort.uniq
+vhost_names = vhost_names.collect { |vhost| "#{vhost}" }.join(" ").split(" ").sort.uniq
 
 #Pull Core rabbit from databag
 rabbitcore = data_bag_item("rabbitmq", "rabbitmq")
