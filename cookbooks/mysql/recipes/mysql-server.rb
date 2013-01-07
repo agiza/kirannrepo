2a9e1042f794f1a2cbed16a30458a5b5f2756cce
Bryan Phinney <bryan.phinney@altisource.com>
2013-01-07 15:42:50 -0500

    Updated to remove mysql-libs and reinstall cronie crontabs sysstat and postfix.

--- cookbooks/mysql/recipes/mysql-server.rb
+++ cookbooks/mysql/recipes/mysql-server.rb
@@ -21,6 +21,10 @@ yum_package "mysql-devel" do
   action :remove
 end
 
+yum_package "mysql-libs" do
+  action :remove
+end
+
 service "mysql" do
   supports :stop => true, :start => true, :restart => true, :reload => true
   action :nothing
@@ -39,6 +43,26 @@ end
 package "MySQL-shared-compat-advanced" do
   action :upgrade
   notifies :restart, resources(:service => "mysql")
+end
+
+package "cronie" do
+  action :upgrade
+end
+
+package "cronie-anacron" do
+  action :upgrade
+end
+
+package "crontabs" do
+  action :upgrade
+end
+
+package "postfix" do
+  action :upgrade
+end
+
+package "sysstat" do
+  action :upgrade
 end
 
 directory "/mysql/data" do
