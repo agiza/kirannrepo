821a45cf579c8265b6bc2c69d0f25af3e773380a
Bryan Phinney <bryan.phinney@altisource.com>
2013-01-08 09:57:53 -0500

    Updated to pull list of mongodb-config servers and add ipaddresses to mongos.

--- cookbooks/mongodb/recipes/shard.rb
+++ cookbooks/mongodb/recipes/shard.rb
@@ -9,28 +9,26 @@
 include_recipe "mongodb::default"
 include_recipe "altisource::altirepo"
 
-#directory "/data" do
-#  owner "mongod"
-#  group "mongod"
-#end
-
-#directory "/data/db" do
-#  owner "mongod"
-#  group "mongod"
-#end
-
 service "mongod" do
   supports :stop => true, :start => true, :restart => true, :status => true, :reload => true
   action :nothing
 end
 
+configserver = []
+configs = search(:node, "role:mongod-config")
+configs.each do |ipaddress|
+  configserver << ipaddress[:ipaddress]
+end
+configserver = configserver.collect { |entry| "#{entry}:27001"}.join(",")
 template "/etc/mongod.conf" do
   source "mongod-shard.conf.erb"
   group "root"
   owner "root"
   mode "0644"
+  variables(:mongodbconfig => configserver)
   notifies :reload, resources(:service => "mongod")
 end
+
 
 service "mongod" do
   action [:enable, :start]
