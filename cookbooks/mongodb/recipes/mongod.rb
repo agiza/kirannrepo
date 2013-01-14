fa674c3cbde51466984c184b4a3712aeb66edb3f
Bryan Phinney <bryan.phinney@altisource.com>
2013-01-14 10:45:42 -0500

    Updated to move scripts from mongod to mongos-mgr recipe.

--- cookbooks/mongodb/recipes/mongod.rb
+++ cookbooks/mongodb/recipes/mongod.rb
@@ -47,54 +47,6 @@ template "/etc/init.d/#{app_name}" do
   notifies :reload, resources(:service => "#{app_name}")
 end
 
-execute "mongod-seed" do
-  command "/etc/mongo/mongod-seed.sh"
-  action :nothing
-end
-
-mongodbnames = data_bag_item("infrastructure", "mongodb")
-template "/etc/mongo/mongod-seed.sh" do
-  source "mongod-seed.sh.erb"
-  owner  "mongod"
-  group  "mongod"
-  mode   "0755"
-  #notifies :run, resources(:execute => "mongod-seed")
-  variables( :mongodb_names => mongodbnames["dbnames"] )
-end
-
-template "/etc/mongo/seedData.js" do
-  source "seedData.js.erb"
-  owner  "mongod"
-  group  "mongod"
-  mode   "0644"
-  variables(
-    :seeddata => mongodbnames['seedData.js']
-  )
-  #notifies :run, resources(:execute => "mongod-seed")
-end
-
-template "/etc/mongo/addIndexes.js" do
-  source "addIndexes.js.erb"
-  owner  "mongod"
-  group  "mongod"
-  mode   "0644"
-  variables(
-    :addindexes => mongodbnames['addIndexes.js']
-  )
-  #notifies :run, resources(:execute => "mongod-seed")
-end
-
-template "/etc/mongo/demoData.js" do
-  source "demoData.js.erb"
-  owner  "mongod"
-  group  "mongod"
-  mode   "0644"
-  variables(
-    :demodata => mongodbnames['demoData.js']
-  )
-  #notifies :run, resources(:execute => "mongod-seed")
-end
-
 service "#{app_name}" do
   action [:enable, :start]
 end
--- cookbooks/mongodb/recipes/mongos-mgr.rb
+++ cookbooks/mongodb/recipes/mongos-mgr.rb
@@ -1,6 +1,6 @@
 #
 # Cookbook Name:: mongodb
-# Recipe:: mongos
+# Recipe:: mongos-mgr
 #
 # Copyright 2012, Altisource
 #
@@ -12,6 +12,54 @@ include_recipe "mongodb::default"
 service "#{app_name}" do
   supports :stop => true, :start => true, :restart => true, :status => true, :reload => true
   action :nothing
+end
+
+execute "mongod-seed" do
+  command "/etc/mongo/mongod-seed.sh"
+  action :nothing
+end
+
+mongodbnames = data_bag_item("infrastructure", "mongodb")
+template "/etc/mongo/mongod-seed.sh" do
+  source "mongod-seed.sh.erb"
+  owner  "mongod"
+  group  "mongod"
+  mode   "0755"
+  #notifies :run, resources(:execute => "mongod-seed")
+  variables( :mongodb_names => mongodbnames["dbnames"] )
+end
+
+template "/etc/mongo/seedData.js" do
+  source "seedData.js.erb"
+  owner  "mongod"
+  group  "mongod"
+  mode   "0644"
+  variables(
+    :seeddata => mongodbnames['seedData.js']
+  )
+  #notifies :run, resources(:execute => "mongod-seed")
+end
+
+template "/etc/mongo/addIndexes.js" do
+  source "addIndexes.js.erb"
+  owner  "mongod"
+  group  "mongod"
+  mode   "0644"
+  variables(
+    :addindexes => mongodbnames['addIndexes.js']
+  )
+  #notifies :run, resources(:execute => "mongod-seed")
+end
+
+template "/etc/mongo/demoData.js" do
+  source "demoData.js.erb"
+  owner  "mongod"
+  group  "mongod"
+  mode   "0644"
+  variables(
+    :demodata => mongodbnames['demoData.js']
+  )
+  #notifies :run, resources(:execute => "mongod-seed")
 end
 
 configserver = []
