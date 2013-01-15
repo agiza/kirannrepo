3d43baa3087c50a9f70178416ba8903c41bc190d
Bryan Phinney <bryan.phinney@altisource.com>
2013-01-15 14:07:48 -0500

    Updated to add recipe for home grown gem server to serve up gems from shared proxy server.

---
+++ cookbooks/infrastructure/recipes/gemserver.rb
@@ -0,0 +1,35 @@
+#
+# Cookbook Name:: infrastructure
+# Recipe:: gemserver
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+
+service "gemserver" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+proxyinfo = data_bag_item("infrastructure", "proxy")
+if proxyinfo.nil? || proxyinfo.empty?
+  Chef::Log.info("No services returned from search.")
+else
+  template "/etc/init.d/gemserver" do
+    source "gemserver-init.erb"
+    owner  "root"
+    group  "root"
+    mode   "0755"
+    variables(
+      :proxyinfo => proxyinfo
+    )
+    notifies :restart, resources(:service => "cntlmd")
+  end
+end
+
+service "gemserver" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
+
