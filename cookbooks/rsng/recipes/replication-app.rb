a39d5d2affd05686a9bbf947ce686243768afa1e
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-07 08:31:38 -0500

    Updated to fix the search to be more resiliant.

--- cookbooks/rsng/recipes/replication-app.rb
+++ cookbooks/rsng/recipes/replication-app.rb
@@ -13,24 +13,28 @@ if node.attribute?('amqpproxy')
   amqphost = node[:amqpproxy].split(":")[0]
   amqpport = node[:amqpproxy].split(":")[1]
 else
-  amqphost = {}
-  search(:node, "role:rabbitserver") do |n|
-    amqphost[n.ipaddress] = {}
+  amqphost = search(:node, "recipes:rabbitmq\\:\\:rabbitmqserver OR role:rabbitserver AND chef_environment:shared")
+  if amqphost.nil? || amqphost.empty?
+    Chef::Log.info("No services returned from search.")
+  else
+    amqphost = amqphost.first
+    amqphost = amqphost["ipaddress"]
+    amqpport = "5672"
   end
-  amqphost = amqphost.first
-  amqpport = "5672"
 end
 
 if node.attribute?('rsngproxy')
   rsnghost = node[:rsngproxy].split(":")[0]
   rsngport = node[:rsngproxy].split(":")[1]
 else
-  rsnghost = {}
-  search(:node, "role:rsng AND chef_environment:#{node.chef_environment}") do |n|
-    rsnghost[n.ipaddress] = {}
+  rsnghost = search(:node, "recipes:rsng\\:\\:rsng-service-app OR role:realservicing AND chef_environment:#{node.chef_environment}")
+  if rsnghost.nil? || rsnghost.empty?
+    Chef::Log.info("No services returned from search.")
+  else
+    rsnghost = rsnghost.first
+    rsnghost = rsnghost["ipaddress"]
+    rsngport = "8080"
   end
-  rsnghost = "#{rsnghost.first}"
-  rsngport = "8080"
 end
 
 service "altitomcat" do
