d75bd400e45ae44c8caa79430cf672358789520d
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-07 06:22:27 -0500

    Updated to make use of nested search.

--- cookbooks/altisource/recipes/altirepo.rb
+++ cookbooks/altisource/recipes/altirepo.rb
@@ -21,7 +21,8 @@ end
 if node.attribute?('yum_server')
   yumserver = node[:yum_server]
 else
-  yumserver = search(:node, 'run_list:recipe\[infrastructure\:\:yumserver\]' && 'run_list:recipe\[github\:\:yum-repo\]') 
+  #yumserver = search(:node, 'run_list:recipe\[infrastructure\:\:yumserver\]' AND 'run_list:recipe\[github\:\:yum-repo\]') 
+  yumserver = search(:node, 'recipes:infrastructure\:\:yumserver' AND 'recipes:github\:\:yum-repo') 
   yumserver = yumserver.first
   yumserver = yumserver["ipaddress"]
 end
