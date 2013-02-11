5382c5ce3b7cb5757015bc0a219dceb87465f059
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-11 15:07:44 -0500

    Updated to try to pull these from node attributes using simple bash script loop.

--- cookbooks/altisource/recipes/volgrp.rb
+++ cookbooks/altisource/recipes/volgrp.rb
@@ -6,27 +6,23 @@
 if node.attribute?("novolume")
   Chef::Log.info("No volume mount attribute is set.")
 else
-  volumes = data_bag_item("infrastructure", "#{node[:vol_name]}volumes")
+  volumes = node[:volumes]
   if volumes.nil? || volumes.empty?
     Chef::Log.info("No volumes found to mount.")
   else
-    volumes.each do |volume|
-      voldata = volume("#{volume}").split("|")
-      execute "#{volume}-mount" do
-        command "/usr/local/bin/#{volume}-mount.sh"
-        action :nothing
-      end
-      template "/usr/local/bin/#{volume}-mount.sh" do
-        source "volgrp-create.erb"
-        group  "root"
-        owner  "root"
-        mode   "0755"
-        variables(
-          :mount => voldata[0],
-          :device => voldata[1]
-        )
-       # notifies :run, "execute[#{volume}-mount]", :delayed
-      end
+    execute "volgrp-mount" do
+      command "/usr/local/bin/volgrp-mount.sh"
+      action :nothing
+    end
+    template "/usr/local/bin/volgrp-mount.sh" do
+      source "volgrp-create.erb"
+      group  "root"
+      owner  "root"
+      mode   "0755"
+      variables(
+        :voldata => volumes
+      )
+     # notifies :run, "execute[volgrp-mount]", :delayed
     end
   end
 end
