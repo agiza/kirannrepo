f9a4de45d97a5b285629e12622cacb811cf65c1e
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-11 07:03:44 -0500

    Updated to try to stopt mongod service if no mongodb-primary recipe is there.

--- cookbooks/mongodb/recipes/replica1.rb
+++ cookbooks/mongodb/recipes/replica1.rb
@@ -54,9 +54,13 @@ template "/etc/init.d/#{app_name}" do
   notifies :reload, resources(:service => "#{app_name}")
 end
 
-#service "mongod" do
-#  action [:disable, :stop]
-#end
+if run_list?("recipe[mongodb::mongod]")
+  Chef::Log.info("This server is a mongodb-primary node.")
+else
+  service "mongod" do
+    action [:disable, :stop]
+  end
+end
 
 service "#{app_name}" do
   action [:enable, :start]
