531e0b2069749a9d15d75f39935cccbe29c5a9ff
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-11 13:58:30 -0500

    Updated to try this again.

--- cookbooks/mysql/attributes/default.rb
+++ cookbooks/mysql/attributes/default.rb
@@ -1,1 +1,1 @@
-default["volumes"] = ["data":"sdb|u02/mysqldata1/data","innodb":"sdc|u02/mysqldata1/innodb","mysqllog":"sdd|u02/mysqldata/mysqllog","tmp":"sde|uo2/mysqldata/tmp","backup":"sdf|u03/mysqldata"]
+default["volumes"] = "{"data":"sdb|u02/mysqldata1/data","innodb":"sdc|u02/mysqldata1/innodb","mysqllog":"sdd|u02/mysqldata/mysqllog","tmp":"sde|uo2/mysqldata/tmp","backup":"sdf|u03/mysqldata"}"
