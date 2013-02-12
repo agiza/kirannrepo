dd1f8e6036696c39084c8b69dd11e253a8d2cca9
chef <phinneyb@altisource.com>
2013-02-12 07:29:36 -0500

    Updated to fix these attributes.

--- cookbooks/mysql/attributes/default.rb
+++ cookbooks/mysql/attributes/default.rb
@@ -1,1 +1,1 @@
-default["volumes"] = "sdb|data|u02/mysqldata1/data sdc|innodb|u02/mysqldata1/innodb sdd|mysqllog|u02/mysqldata/mysqllog sde|tmp|u02/mysqldata/tmp sdf|mysqldata|u03/mysqldata"
+default["volumes"] = "sdb|data|u02/mysqldata1/data sdc|innodb|u02/mysqldata1/innodb sdd|mysqllog|u02/mysqldata/mysqllog sde|tmp|u02/mysqldata/tmp sdf|backup|u03/mysqldata"
