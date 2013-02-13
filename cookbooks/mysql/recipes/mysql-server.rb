cb0b689a57deaca5c2cfe837d3f0f091984a225e
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-13 13:18:23 -0500

    Updated to try to consolidate the recipe for mysql server.

--- cookbooks/mysql/recipes/mysql-server.rb
+++ cookbooks/mysql/recipes/mysql-server.rb
@@ -20,25 +20,25 @@ execute "mysql-dbd" do
   action :nothing
 end
 
-package "MySQL-shared-compat-advanced" do
+package "MySQL-shared-compat-advanced", "MySQL-client-advanced", "MySQL-server-advanced", "MySQL-devel-advanced", "MySQL-test-advanced" do
   action :upgrade
 end
 
-package "MySQL-client-advanced" do
-  action :upgrade
-end
+#package "MySQL-client-advanced" do
+#  action :upgrade
+#end
 
-package "MySQL-server-advanced" do
-  action :upgrade
-end
+#package "MySQL-server-advanced" do
+#  action :upgrade
+#end
 
-package "MySQL-devel-advanced" do
-  action :upgrade
-end
+#package "MySQL-devel-advanced" do
+#  action :upgrade
+#end
 
-package "MySQL-test-advanced" do
-  action :upgrade
-end
+#package "MySQL-test-advanced" do
+#  action :upgrade
+#end
 
 link "/usr/lib64/libmysqlclient.so" do
   to "/usr/lib64/libmysqlclient.so.16.0.0"
@@ -59,66 +59,72 @@ if yumserver.nil? || yumserver.empty?
 else
   yumserver = yumserver.first
   yumserver = yumserver["ipaddress"]
-end
-template "/usr/local/bin/mysql-dbd" do
-  source "mysql-dbd.erb"
-  owner  "root"
-  group  "root"
-  mode   "0755"
-  variables(
-    :dbdapp => dbdapp,
-    :yumserver => yumserver
-  )
-  notifies :run, resources(:execute => "mysql-dbd"), :delayed
+  template "/usr/local/bin/mysql-dbd" do
+    source "mysql-dbd.erb"
+    owner  "root"
+    group  "root"
+    mode   "0755"
+    variables(
+      :dbdapp => dbdapp,
+      :yumserver => yumserver
+    )
+    notifies :run, resources(:execute => "mysql-dbd"), :delayed
+  end
 end
 
-directory "/mysql" do
-  owner "mysql"
-  group "mysql"
+directory "/mysql", "/mysql/data", "/mysql/log", "/mysql/log/err", "/mysql/log/slow", "/mysql/log/general", "/mysql/tmp", "/mysql/innodb" do
+  owner  "mysql"
+  group  "mysql"
   action :create
 end
 
-directory "/mysql/data" do
-  owner "mysql"
-  group "mysql"
-  action :create
-end
+#directory "/mysql" do
+#  owner "mysql"
+#  group "mysql"
+#  action :create
+#end
+#
+#directory "/mysql/data" do
+#  owner "mysql"
+#  group "mysql"
+#  action :create
+#end
+#
+#directory "/mysql/log" do
+#  owner "mysql"
+#  group "mysql"
+#  action :create
+#end
+#
+#directory "/mysql/log/err" do
+#  owner "mysql"
+#  group "mysql"
+#  action :create
+#end
 
-directory "/mysql/log" do
-  owner "mysql"
-  group "mysql"
-  action :create
-end
+#directory "/mysql/log/slow" do
+#  owner "mysql"
+#  group "mysql"
+#  action :create
+#end
+#
+#directory "/mysql/log/general" do
+#  owner "mysql"
+#  group "mysql"
+#  action :create
+#end
+#
+#directory "/mysql/tmp" do
+#  owner "mysql"
+#  group "mysql" 
+#  action :create 
+#end
 
-directory "/mysql/log/err" do
-  owner "mysql"
-  group "mysql"
-  action :create
-end
-
-directory "/mysql/log/slow" do
-  owner "mysql"
-  group "mysql"
-  action :create
-end
-
-directory "/mysql/log/general" do
-  owner "mysql"
-  group "mysql"
-  action :create
-end
-
-directory "/mysql/tmp" do
-  owner "mysql"
-  group "mysql" 
-  action :create 
-end
-
-directory "/mysql/innodb" do
-  owner "mysql"
-  group "mysql"
-  action :create
-end
+#directory "/mysql/innodb" do
+#  owner "mysql"
+#  group "mysql"
+#  action :create
+#end
 
 service "mysql" do
   supports :stop => true, :start => true, :restart => true, :reload => true
