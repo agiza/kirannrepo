eb990b884b127994e9fb5aa64b78bd5c736e2f88
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-14 13:42:11 -0500

    updated to try to make the recipe more modular.

--- cookbooks/rabbitmq/recipes/default.rb
+++ cookbooks/rabbitmq/recipes/default.rb
@@ -7,3 +7,18 @@
 # All rights reserved - Do Not Redistribute
 #
 
+include_recipe "altisource::altirepo"
+include_recipe "altisource::epel-local"
+include_recipe "infrastructure::selinux"
+
+package "rabbitmq-server" do
+  #provider Chef::Provider::Package::Yum
+  action :upgrade
+end
+
+execute "rabbit-plugins" do
+  command "rabbitmq-plugins enable rabbitmq_stomp; rabbitmq-plugins enable rabbitmq_management"
+  action :run
+end
+
+
