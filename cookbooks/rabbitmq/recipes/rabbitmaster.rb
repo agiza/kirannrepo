3d4d72095b6e9c565f8eb035fe03b461b5e2ef91
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-14 14:22:51 -0500

    Updated to try this sort again.

--- cookbooks/rabbitmq/recipes/rabbitmaster.rb
+++ cookbooks/rabbitmq/recipes/rabbitmaster.rb
@@ -102,7 +102,7 @@ rabbitapps.each do |app|
     vhost_names << appvhosts
   end
 end
-vhost_names = vhost_names.collect { |vhost| "#{vhost}" }.sort.uniq.join(" ")
+vhost_names = vhost_names.collect { |vhost| "#{vhost}" }.sort.uniq.split(" ").join(" ")
 
 # This defines the common service that creates the initial cluster.
 execute "rabbit-config" do
