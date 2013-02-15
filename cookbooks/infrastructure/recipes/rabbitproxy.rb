7d6d5945d4419aa8d5c3f96719031b0c6a95d25e
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-15 07:09:19 -0500

    Updated to try to streamline this recipe.

--- cookbooks/infrastructure/recipes/rabbitproxy.rb
+++ cookbooks/infrastructure/recipes/rabbitproxy.rb
@@ -8,64 +8,19 @@
 #
 include_recipe "infrastructure::haproxy"
 
-if node.attribute?('amqpproxy')
-  amqphost = node[:amqpproxy].split(":")[0]
-  amqpport = node[:amqpproxy].split(":")[1]
-else
-  amqphost = {}
-  if node.attribute?('performance')
-    search(:node, "role:rabbitserver AND chef_environment:#{node.chef_environment}") do |n|
-      amqphost[n.ipaddress] = {}
-    end
-  else
-    search(:node, "role:rabbitserver AND chef_environment:shared") do |n|
-      amqphost[n.ipaddress] = {}
-    end
-  end
-  amqphost = amqphost.first
-  amqpport = "5672"
-end
-
-if node.attribute?('stompproxy')
-  stomphost = node[:stompproxy].split(":")[0]
-  stompport = node[:stompproxy].split(":")[1]
-else
-  stomphost = {}
-  if node.attribute?('performance')
-    search(:node, "role:rabbitserver AND chef_environment:#{node.chef_environment}") do |n|
-      stomphost[n.ipaddress] = {}
-    end
-  else
-    search(:node, "role:rabbitserver AND chef_environment:shared") do |n|
-      stomphost[n.ipaddress] = {}
-    end
-  end
-  stomphost = stomphost.first
-  stompport = "61613"
-end
-
-package "haproxy" do
-  action :upgrade
-end
-
-service "haproxy" do
-  supports :stop => true, :start => true, :restart => true, :reload => true
-  action :nothing
-end
-
-clusternodes = {}
+# Check for stress/performance environment as infrastructure may be separate there.
 if node.attribute?('performance')
-  search(:node, "role:rabbitserver AND chef_environment:#{node.chef_environment}") do |n|
-    clusternodes[n.ipaddress] = {}
-  end
+  environment = node[:chef_environment]
 else
-  search(:node, "role:rabbitserver AND chef_environment:shared") do |n|
-    clusternodes[n.ipaddress] = {}
-  end
+  environment = "shared"
 end
-if clusternodes.nil? || clusternodes.empty?
-  Chef::Log.info("No services returned from search.")
+clusternodes = []
+rabbitnodes = search(:node, "role:rabbitserver AND chef_environment:#{environment}")
+if rabbitnodes.nil? || rabbitnodes.empty?
+  Chef::Log.warn("Unable to find any rabbitservers in the infrastructure.") && rabbitnodes = "None"
 else
+  clusternodes << rabbitnodes["ipaddress"]
+  clusternodes = clusternodes.sort.uniq
   template "/etc/haproxy/haproxy.cfg" do
     source "rabbitproxy.cfg.erb"
     group "haproxy"
@@ -73,8 +28,8 @@ else
     mode "0644"
     variables(
       :clusternodes => clusternodes,
-      :amqpport => "#{amqpport}",
-      :stompport => "#{stompport}"
+      :amqpport => "5672",
+      :stompport => "61613"
     )
     notifies :restart, resources(:service => "haproxy")
   end
