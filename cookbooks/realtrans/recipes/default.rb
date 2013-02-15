7609421c5effca5e2f9b5589345ba17591f0fe70
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-15 10:58:24 -0500

    Updated to try to move some common code to default recipe.

--- cookbooks/realtrans/recipes/default.rb
+++ cookbooks/realtrans/recipes/default.rb
@@ -5,4 +5,18 @@
 
 #include_recipe "java"
 
+# This looks for realdoc proxy attribute and allows override of realdoc server or finds the first server itself
+if node.attribute?('realdocproxy')
+  rdochost = node[:realdocproxy].split(":")[0]
+  rdocport = node[:realdocproxy].split(":")[1]
+else
+  rdochost = search(:node, "recipes:realdoc\\:\\:realdoc OR role:realdoc AND chef_environment:#{node.chef_environment}")
+    if rdochost.nil? || rdochost.empty?
+    Chef::Log.warn("No services returned from search.") && rdochost = "No servers found."
+  else
+    rdochost = rdochost.first
+    rdochost = rdochost["ipaddress"]
+    rdocport = "8080"
+  end
+end
 
--- cookbooks/realtrans/recipes/realtrans-central.rb
+++ cookbooks/realtrans/recipes/realtrans-central.rb
@@ -27,20 +27,22 @@ end
 # Include tomcat recipe to get tomcat installed
 include_recipe "altisource::altitomcat"
 
+include_recipe "realtrans::default"
 # This looks for realdoc proxy attribute and allows override of realdoc server or finds the first server itself
-if node.attribute?('realdocproxy')
-  rdochost = node[:realdocproxy].split(":")[0]
-  rdocport = node[:realdocproxy].split(":")[1]
-else
-  rdochost = search(:node, "recipes:realdoc\\:\\:realdoc OR role:realdoc AND chef_environment:#{node.chef_environment}")
-    if rdochost.nil? || rdochost.empty?
-    Chef::Log.warn("No services returned from search.") && rdochost = "No servers found."
-  else
-    rdochost = rdochost.first
-    rdochost = rdochost["ipaddress"]
-    rdocport = "8080"
-  end
-end
+#if node.attribute?('realdocproxy')
+#  rdochost = node[:realdocproxy].split(":")[0]
+#  rdocport = node[:realdocproxy].split(":")[1]
+#else
+#  rdochost = search(:node, "recipes:realdoc\\:\\:realdoc OR role:realdoc AND chef_environment:#{node.chef_environment}")
+#    if rdochost.nil? || rdochost.empty?
+#    Chef::Log.warn("No services returned from search.") && rdochost = "No servers found."
+#  else
+#    rdochost = rdochost.first
+#    rdochost = rdochost["ipaddress"]
+#    rdocport = "8080"
+#  end
+#end
+
 # This looks for rt central proxy attribute or finds the first server itself.
 if node.attribute?('rtcenproxy')
   rtcenhost = node[:rtcenproxy].split(":")[0]
