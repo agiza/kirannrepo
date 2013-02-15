524fabef9fcc27eabe24221bdad11203bb81e1de
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-15 11:42:15 -0500

    Updated to move common code to default recipe.

--- cookbooks/integration/recipes/int-collateralanalytics.rb
+++ cookbooks/integration/recipes/int-collateralanalytics.rb
@@ -27,19 +27,16 @@ else
 end
 
 include_recipe "altisource::altitomcat"
-if node.attribute?('amqpproxy')
-  amqphost = node[:amqpproxy].split(":")[0]
-  amqpport = node[:amqpproxy].split(":")[1]
-else
-  amqphost = search(:node, "recipes:rabbitmq\\:\\:rabbitmqserver OR role:rabbitserver AND chef_environment:shared")
-  if amqphost.nil? || amqphost.empty?
-    Chef::Log.warn("No services returned from search.") && amqphost = "No servers found."
-  else
-    amqphost = amqphost.first
-    amqphost = amqphost["ipaddress"]
-    amqpport = "5672"
-  end
-end
+
+include_recipe "realtrans::default"
+rdochost = node[:rdochost]
+rdocport = node[:rdocport]
+
+rtcenhost = node[:rtcenhost]
+rtcenport = node[:rtcenport]
+
+amqphost = node[:amqphost]
+amqpport = node[:amqpport]
 
 service "altitomcat" do
   supports :stop => true, :start => true, :restart => true, :reload => true
--- cookbooks/l1/recipes/l1-central.rb
+++ cookbooks/l1/recipes/l1-central.rb
@@ -25,48 +25,13 @@ else
 end
 
 include_recipe "altisource::altitomcat"
-
-if node.attribute?('realdocproxy')
-  rdochost = node[:realdocproxy].split(":")[0]
-  rdocport = node[:realdocproxy].split(":")[1]
-else
-  rdochost = search(:node, "recipes:realdoc\\:\\:realdoc OR role:realdoc AND chef_environment:#{node.chef_environment}")
-  if rdochost.nil? || rdochost.empty?
-    Chef::Log.warn("No services returned from search.") && rdochost = "No servers found."
-  else
-    rdochost = rdochost.first
-    rdochost = rdochost["ipaddress"]
-    rdocport = "8080"
-  end
-end
-
-if node.attribute?('l1cenproxy')
-  l1cenhost = node[:l1cenproxy].split(":")[0]
-  l1cenport = node[:l1cenproxy].split(":")[1]
-else
-  l1cenhost = search(:node, "recipes:l1\\:\\:l1-central OR role:l1-cen AND chef_environment:#{node.chef_environment}")
-  if l1cenhost.nil? || l1cenhost.empty?
-    Chef::Log.warn("No services returned from search.") && l1cenhost = "No servers found."
-  else
-    l1cenhost = l1cenhost.first
-    l1cenhost = l1cenhost["ipaddress"]
-    l1cenport = "8080"
-  end
-end
-
-if node.attribute?('amqpproxy')
-  amqphost = node[:amqpproxy].split(":")[0]
-  amqpport = node[:amqpproxy].split(":")[1]
-else
-  amqphost = search(:node, "recipes:rabbitmq\\:\\:rabbitmqserver OR role:rabbitserver AND chef_environment:shared")
-  if amqphost.nil? || amqphost.empty?
-    Chef::Log.warn("No services returned from search.") && amqphost = "No servers found."
-  else
-    amqphost = amqphost.first
-    amqphost = amqphost["ipaddress"]
-    amqpport = "5672"
-  end
-end
+include_recipe "l1::default"
+rdochost = node[:rdochost]
+rdocport = node[:rdocport]
+l1cenhost = node[:l1cenhost]
+l1cenport = node[:l1cenport]
+amqphost = node[:amqphost]
+amqpport = node[:amqpport]
 
 service "altitomcat" do
   supports :stop => true, :start => true, :restart => true, :reload => true
