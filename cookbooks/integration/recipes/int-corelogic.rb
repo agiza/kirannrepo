efb307dd05c92e949da1103bca14813f541f321b
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-15 11:45:01 -0500

    updated to include l1 and refer to default for rabbit on integration adapters.

--- cookbooks/integration/recipes/int-corelogic.rb
+++ cookbooks/integration/recipes/int-corelogic.rb
@@ -28,19 +28,9 @@ end
 
 
 include_recipe "altisource::altitomcat"
-if node.attribute?('amqpproxy')
-  amqphost = node[:amqpproxy].split(":")[0]
-  amqpport = node[:amqpproxy].split(":")[1]
-else
-  amqphost = search(:node, "recipes:rabbitmq\\:\\:rabbitmqserver OR role:rabbitserver AND chef_environment:shared")
-  if amqphost.nil? || amqphost.empty?
-    Chef::Log.warn("No services returned from search.") && amqphost = "No servers found."
-  else
-    amqphost = amqphost.first
-    amqphost = amqphost["ipaddress"]
-    amqpport = "5672"
-  end
-end
+include_recipe "l1::default"
+amqphost = node[:amqphost]
+amqpport = node[:amqpport]
 
 service "altitomcat" do
   supports :stop => true, :start => true, :restart => true, :reload => true
