9c3a95be90d9c9cba65a4ec766765df8e7abbad7
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-19 10:45:57 -0500

    Updated to add a new LWRP provider to check network connections, must pass IP and port in same value.

---
+++ cookbooks/altisource/providers/netcheck.rb
@@ -0,0 +1,13 @@
+action :prep do
+  execute "prep" do
+    only_if "which nc | grep 'which: no nc in'"
+    command "yum install -y nc"
+  end
+end
+
+action :check do
+  execute "check" do
+    command "nc -zw2 -v #{new_resource.name}"
+  end
+end
+
