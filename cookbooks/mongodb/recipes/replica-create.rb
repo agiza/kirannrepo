0794283394385ec3ed2f627b7a8b18e03a0dc141
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-21 10:21:46 -0500

    Updated to try to fix the performance environment detection.

--- cookbooks/mongodb/recipes/replica-create.rb
+++ cookbooks/mongodb/recipes/replica-create.rb
@@ -16,7 +16,7 @@ end
 
 replicaset = node[:replicaset]
 replicalist = []
-if node.environment?('performance')
+if node.chef_environment?('performance')
   replicas = search(:node, "recipes:mongodb\\:\\:mongod OR role:mongodb-primary AND role:mongodb-#{replicaset} AND chef_environment:#{node.chef_environment}")
   replicas.each do |replica|
     replicalist << "#{replica[:ipaddress]}:27017"
