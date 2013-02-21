0f315c408e94390b10f52c2dc11612f3164b7ba9
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-21 10:18:13 -0500

    updated to add a recipe to create the initial shard, must be run from a mongos instance.

---
+++ cookbooks/mongodb/recipes/shard-enable.rb
@@ -0,0 +1,52 @@
+#
+# Cookbook Name:: mongodb
+# Recipe:: shard-enable
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+
+execute "shard-enable" do
+  command "/usr/local/bin/shard-enable"
+  action :nothing
+end
+
+replicaset = node[:replicaset]
+replicalist = []
+if node.environment?('performance')
+  replicas = search(:node, "recipes:mongodb\\:\\:mongod OR role:mongodb-primary AND role:mongodb-#{replicaset} AND chef_environment:#{node.chef_environment}")
+  replicas.each do |replica|
+    replicalist << "#{replica[:ipaddress]}:27017"
+  end
+else
+  replicas = search(:node, "recipes:mongodb\\:\\:mongod OR role:mongodb-primary AND role:mongodb-#{replicaset} AND chef_environment:shared")
+  replicas.each do |replica|
+    replicalist << "#{replica[:ipaddress]}:27017"
+  end
+end
+template "/etc/mongo/shardadd.js" do
+  source "shardadd.js.erb"
+  owner  "root"
+  group  "root"
+  mode   "0644"
+  variables(
+    :replicas => replicalist
+  )
+  notifies :run, 'execute[shard-enable]'
+end
+
+template "/usr/local/bin/shard-enable" do
+  source "shard-enable.erb"
+  owner  "root"
+  group  "root"
+  mode   "0755"
+  notifies :run, 'execute[shard-enable]'
+end
+
+ruby_block "remove shard-enable from run list" do
+  block do
+    node.run_list.remove("mongodb::shard-enable")
+  end
+end
+
