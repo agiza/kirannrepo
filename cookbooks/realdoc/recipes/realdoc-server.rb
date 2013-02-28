369d8a134b2bfb43a91342b05f28c48c58c87885
Bryan Phinney <bryan.phinney@altisource.com>
2013-02-28 08:56:34 -0500

    Updated to fix the templates and recipes.

--- cookbooks/realdoc/recipes/realdoc-server.rb
+++ cookbooks/realdoc/recipes/realdoc-server.rb
@@ -7,6 +7,6 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "realdoc::realdoc"
-include_recipe "realdoc::jdbc-provider"
+include_recipe "realdoc::jdbc-data-provider"
 include_recipe "realdoc::realsvc-recon-adapter"
 
