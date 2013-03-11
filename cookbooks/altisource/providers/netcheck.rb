46cc15ebf5cf16302faa42c11bd8ba51091b60c4
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-11 13:48:15 -0400

    Updated to add an error message.

--- cookbooks/altisource/providers/netcheck.rb
+++ cookbooks/altisource/providers/netcheck.rb
@@ -4,10 +4,11 @@ action :prep do
     command "yum install -y nc"
   end
 end
-
-action :check do
-  execute "check" do
-    command "nc -zw2 -v #{new_resource.name} #{new_resource.port}"
+begin
+  action :check do
+    execute "check" do
+      command "nc -zw2 -v #{new_resource.name} #{new_resource.port}"
+    end
   end
+  raise "Network check for #{new_resource.name} on port #{new_resource.port} failed.  Either the network is not up, or the service is not responding."
 end
-
