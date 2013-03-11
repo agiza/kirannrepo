37650c8833423546b76975aa3241e8d46bfaf086
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-11 10:46:24 -0400

    Updated to try to fix the port.

--- cookbooks/altisource/resources/network.rb
+++ cookbooks/altisource/resources/network.rb
@@ -1,5 +1,5 @@
 actions :prep, :check
 
 attribute :name, :kind_of => String, :name_attribute => true
-attribute :port, :kind_of => String
+attribute :port, :kind_of => String, :port_attribute => true
 #attribute :type, :kind_of => String
