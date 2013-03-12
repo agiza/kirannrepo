d0f0152fd7b0a82891bf05c0bc55cd6c841798d6
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-12 09:27:21 -0400

    updated to try to add a whyrun mode.

--- cookbooks/altisource/providers/netcheck.rb
+++ cookbooks/altisource/providers/netcheck.rb
@@ -1,3 +1,10 @@
+def whyrun_supported
+  true
+end
+
+def whyrun_mode?
+  Chef::Config[:whyrun]
+end
 action :prep do
   execute "prep" do
     only_if "which nc | grep 'which: no nc in'"
@@ -8,6 +15,6 @@ action :check do
   execute "check" do
     command "nc -zw2 -v #{new_resource.name} #{new_resource.port}"
     ignore_failure true
-    Chef::Log.info("Result of network test of #{new_resource.name} on port #{new_resource.port}", return_code)
+    Chef::Log.info("Network test of #{new_resource.name} on port #{new_resource.port}")
   end
 end
