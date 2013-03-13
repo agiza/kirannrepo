f343791d508b5cebf7c10a0ed5a06bc1a887d4c7
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-13 13:37:34 -0400

    Updated to loop through all environments to try that out.

--- cookbooks/apache/recipes/rtvhost.rb
+++ cookbooks/apache/recipes/rtvhost.rb
@@ -7,57 +7,59 @@
 # All rights reserved - Do Not Redistribute
 #
 # Create an array of all environments with realtrans workers installed
-rtenvirons = []
-%w[realtrans-fp realtrans-rp realtrans-reg realtrans-vp realtrans-server].each do |app|
-  Chef::Log.info("working on #{app}")
-  search(:node, "recipes:*\\:\\:#{app}").each do |worker|
-    Chef::Log.info("found #{worker}")
-    rtenvirons << worker[:chef_environment] unless worker[:chef_environment].nil? || worker[:chef_environment].empty?
-    Chef::Log.info("#{worker[:chef_environment]} added.")
-  end
-end
+#rtenvirons = []
+#%w[realtrans-fp realtrans-rp realtrans-reg realtrans-vp realtrans-server].each do |app|
+#  Chef::Log.info("working on #{app}")
+#  search(:node, "recipes:*\\:\\:#{app}").each do |worker|
+#    Chef::Log.info("found #{worker}")
+#    rtenvirons << worker[:chef_environment] unless worker[:chef_environment].nil? || worker[:chef_environment].empty?
+#    Chef::Log.info("#{worker[:chef_environment]} added.")
+#  end
+#end
 
-if rtenvirons.nil? || rtenvirons.empty?
-  Chef::Log.info("No realtrans installations found in any environment.")
-else
-  # Databag item for webserver hostname
-  webName = data_bag_item("infrastructure", "apache")
-  if node.attribute?('ssl_force')
-    ssl = ".ssl"
+node[:chef_environment].each do |rtenvirons|
+
+  if rtenvirons.nil? || rtenvirons.empty?
+    Chef::Log.info("No realtrans installations found in any environment.")
   else
-    ssl = ""
-  end
-  serveripallow = webName['serveripallow'].split("|")
+    # Databag item for webserver hostname
+    webName = data_bag_item("infrastructure", "apache")
+    if node.attribute?('ssl_force')
+      ssl = ".ssl"
+    else
+      ssl = ""
+    end
+    serveripallow = webName['serveripallow'].split("|")
 
-  # Convert the hash list of environments into a string, unique values, then split
-  rtenvirons = rtenvirons.sort.uniq
+    # Convert the hash list of environments into a string, unique values, then split
+    rtenvirons = rtenvirons.sort.uniq
 
-  # Loop through list of environments to build workers and pass to the vhost/proxy templates
-  rtenvirons.each do |environ|
-    begin
-      fpworkers = search(:node, "recipes:*\\:\\:realtrans-fp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}")
-      rescue Net::HTTPServerException
-        raise "Unable to find realtrans-fp workers in #{environ}"
-    end
-    fpnames = fpworkers["ipaddress"].sort.uniq unless fpnames.nil? || fpnames.empty?
-    begin
-      rpworkers = search(:node, "recipes:*\\:\\:realtrans-rp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}")
-      rescue Net::HTTPServerException
-        raise "Unable to find realtrans-rp workers in #{environ}"
-    end
-    rpnames = rpworkers["ipaddress"].sort.uniq unless fpnames.nil? || fpnames.empty?
-    begin
-      vpworkers = search(:node, "recipes:*\\:\\:realtrans-vp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}")
-      rescue Net::HTTPServerException
-        raise "Unable to find realtrans-vp workers in #{environ}"
-    end
-    vpnames = vpworkers["ipaddress"].sort.uniq unless vpnames.nil? || vpnames.empty?
-    begin
-      regworkers = search(:node, "recipes:*\\:\\:realtrans-reg AND chef_environment:#{environ}" || "recipes:8\\:\\:realtrans-server AND chef_environment:#{environ}")
-      rescue Net::HTTPServerException
-        raise "Unable to find realtrans-reg workers in #{environ}"
-    end
-    regnames = regworkers["ipaddress"].sort.uniq unless regnames.nil? || regnames.empty?
+    # Loop through list of environments to build workers and pass to the vhost/proxy templates
+    rtenvirons.each do |environ|
+      begin
+        fpworkers = search(:node, "recipes:*\\:\\:realtrans-fp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}")
+        rescue Net::HTTPServerException
+          raise "Unable to find realtrans-fp workers in #{environ}"
+      end
+      fpnames = fpworkers["ipaddress"].sort.uniq unless fpnames.nil? || fpnames.empty?
+      begin
+        rpworkers = search(:node, "recipes:*\\:\\:realtrans-rp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}")
+        rescue Net::HTTPServerException
+          raise "Unable to find realtrans-rp workers in #{environ}"
+      end
+      rpnames = rpworkers["ipaddress"].sort.uniq unless fpnames.nil? || fpnames.empty?
+      begin
+        vpworkers = search(:node, "recipes:*\\:\\:realtrans-vp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}")
+        rescue Net::HTTPServerException
+          raise "Unable to find realtrans-vp workers in #{environ}"
+      end
+      vpnames = vpworkers["ipaddress"].sort.uniq unless vpnames.nil? || vpnames.empty?
+      begin
+        regworkers = search(:node, "recipes:*\\:\\:realtrans-reg AND chef_environment:#{environ}" || "recipes:8\\:\\:realtrans-server AND chef_environment:#{environ}")
+        rescue Net::HTTPServerException
+          raise "Unable to find realtrans-reg workers in #{environ}"
+      end
+      regnames = regworkers["ipaddress"].sort.uniq unless regnames.nil? || regnames.empty?
 #    fpnames = []
 #    rpnames = []
 #    vpnames = []
@@ -74,36 +76,37 @@ else
 #    search(:node, "recipes:*\\:\\:realtrans-reg OR realtransreg_version:* AND chef_environment:#{environ}").each do |worker|
 #      regnames << worker["ipaddress"]
 #    end
-    template "/etc/httpd/proxy.d/rt-#{environ}.proxy.conf" do
-      source "rt.proxy.conf.erb"
-      owner  "root"
-      group  "root"
-      mode   "0644"
-      notifies :reload, resources(:service => "httpd")
-      variables(
-        :fpworkers => fpnames,
-        :rpworkers => rpnames,
-        :vpworkers => vpnames,
-        :regworkers => regnames,
-        :vhostName => "#{environ}",
-        :environ => "#{environ}",
-        :serveripallow => serveripallow
-      )
-    end
-    template "/etc/httpd/conf.d/rt-#{environ}.vhost.conf" do
-      source "rtvhost#{ssl}.conf.erb"
-      owner  "root"
-      group  "root"
-      mode   "0644"
-      notifies :reload, resources(:service => "httpd")
-      variables(
-        :vhostName => "#{environ}",
-        :serverName => webName["rt#{environ}"]
-      )
-    end
-    directory "/var/www/html/#{environ}" do
-      owner "root"
-      group "root"
+      template "/etc/httpd/proxy.d/rt-#{environ}.proxy.conf" do
+        source "rt.proxy.conf.erb"
+        owner  "root"
+        group  "root"
+        mode   "0644"
+        notifies :reload, resources(:service => "httpd")
+        variables(
+          :fpworkers => fpnames,
+          :rpworkers => rpnames,
+          :vpworkers => vpnames,
+          :regworkers => regnames,
+          :vhostName => "#{environ}",
+          :environ => "#{environ}",
+          :serveripallow => serveripallow
+        )
+      end
+      template "/etc/httpd/conf.d/rt-#{environ}.vhost.conf" do
+        source "rtvhost#{ssl}.conf.erb"
+        owner  "root"
+        group  "root"
+        mode   "0644"
+        notifies :reload, resources(:service => "httpd")
+        variables(
+          :vhostName => "#{environ}",
+          :serverName => webName["rt#{environ}"]
+        )
+      end
+      directory "/var/www/html/#{environ}" do
+        owner "root"
+        group "root"
+      end
     end
   end
 end
