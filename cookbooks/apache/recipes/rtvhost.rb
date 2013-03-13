9346331496c6cc1444dadf11bbc59bb13084cd5f
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-13 17:20:54 -0400

    Updated to remove extra end loop.

--- cookbooks/apache/recipes/rtvhost.rb
+++ cookbooks/apache/recipes/rtvhost.rb
@@ -35,56 +35,55 @@ else
   serveripallow = webName['serveripallow'].split("|")
 
   # Loop through list of environments to build workers and pass to the vhost/proxy templates
-    fpnames = []
-    rpnames = []
-    vpnames = []
-    regnames = []
-    search(:node, "recipes:*\\:\\:realtrans-fp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
-      fpnames << worker["ipaddress"] unless worker.nil? || worker.empty?
-    end
-    search(:node, "recipes:*\\:\\:realtrans-rp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
-      rpnames << worker["ipaddress"] unless worker.nil? || worker.empty?
-    end
-    search(:node, "recipes:*\\:\\:realtrans-vp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
-      vpnames << worker["ipaddress"] unless worker.nil? || worker.empty?
-    end
-    search(:node, "recipes:*\\:\\:realtrans-reg AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
-      regnames << worker["ipaddress"] unless worker.nil? || worker.empty?
-    end
-    fpnames = fpnames.sort.uniq unless fpnames.nil? || fpnames.empty?
-    rpnames = rpnames.sort.uniq unless rpnames.nil? || rpnames.empty?
-    vpnames = vpnames.sort.uniq unless vpnames.nil? || vpnames.empty?
-    regnames = regnames.sort.uniq unless regnames.nil? || regnames.empty?
-    template "/etc/httpd/proxy.d/rt-#{environ}.proxy.conf" do
-      source "rt.proxy.conf.erb"
-      owner  "root"
-      group  "root"
-      mode   "0644"
-      notifies :reload, resources(:service => "httpd")
-      variables(
-        :fpworkers => fpnames.sort.uniq,
-        :rpworkers => rpnames.sort.uniq,
-        :vpworkers => vpnames.sort.uniq,
-        :regworkers => regnames.sort.uniq,
-        :vhostName => "#{environ}",
-        :environ => "#{environ}",
-        :serveripallow => serveripallow
-      )
-    end
-    template "/etc/httpd/conf.d/rt-#{environ}.vhost.conf" do
-      source "rtvhost#{ssl}.conf.erb"
-      owner  "root"
-      group  "root"
-      mode   "0644"
-      notifies :reload, resources(:service => "httpd")
-      variables(
-        :vhostName => "#{environ}",
-        :serverName => webName["rt#{environ}"]
-      )
-    end
-    directory "/var/www/html/#{environ}" do
-      owner "root"
-      group "root"
-    end
+  fpnames = []
+  rpnames = []
+  vpnames = []
+  regnames = []
+  search(:node, "recipes:*\\:\\:realtrans-fp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
+    fpnames << worker["ipaddress"] unless worker.nil? || worker.empty?
+  end
+  search(:node, "recipes:*\\:\\:realtrans-rp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
+    rpnames << worker["ipaddress"] unless worker.nil? || worker.empty?
+  end
+  search(:node, "recipes:*\\:\\:realtrans-vp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
+    vpnames << worker["ipaddress"] unless worker.nil? || worker.empty?
+  end
+  search(:node, "recipes:*\\:\\:realtrans-reg AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
+    regnames << worker["ipaddress"] unless worker.nil? || worker.empty?
+  end
+  fpnames = fpnames.sort.uniq unless fpnames.nil? || fpnames.empty?
+  rpnames = rpnames.sort.uniq unless rpnames.nil? || rpnames.empty?
+  vpnames = vpnames.sort.uniq unless vpnames.nil? || vpnames.empty?
+  regnames = regnames.sort.uniq unless regnames.nil? || regnames.empty?
+  template "/etc/httpd/proxy.d/rt-#{environ}.proxy.conf" do
+    source "rt.proxy.conf.erb"
+    owner  "root"
+    group  "root"
+    mode   "0644"
+    notifies :reload, resources(:service => "httpd")
+    variables(
+      :fpworkers => fpnames.sort.uniq,
+      :rpworkers => rpnames.sort.uniq,
+      :vpworkers => vpnames.sort.uniq,
+      :regworkers => regnames.sort.uniq,
+      :vhostName => "#{environ}",
+      :environ => "#{environ}",
+      :serveripallow => serveripallow
+    )
+  end
+  template "/etc/httpd/conf.d/rt-#{environ}.vhost.conf" do
+    source "rtvhost#{ssl}.conf.erb"
+    owner  "root"
+    group  "root"
+    mode   "0644"
+    notifies :reload, resources(:service => "httpd")
+    variables(
+      :vhostName => "#{environ}",
+      :serverName => webName["rt#{environ}"]
+    )
+  end
+  directory "/var/www/html/#{environ}" do
+    owner "root"
+    group "root"
   end
 end
