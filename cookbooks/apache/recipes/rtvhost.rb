0bb0b6b045fc1fd79da2fd93720f571c73b9708a
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-13 17:46:44 -0400

    Updated to fix the environment loop.

--- cookbooks/apache/recipes/rtvhost.rb
+++ cookbooks/apache/recipes/rtvhost.rb
@@ -33,57 +33,58 @@ else
     ssl = ""
   end
   serveripallow = webName['serveripallow'].split("|")
-
-  # Loop through list of environments to build workers and pass to the vhost/proxy templates
-  fpnames = []
-  rpnames = []
-  vpnames = []
-  regnames = []
-  search(:node, "recipes:*\\:\\:realtrans-fp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
-    fpnames << worker["ipaddress"]
-  end
-  search(:node, "recipes:*\\:\\:realtrans-rp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
-    rpnames << worker["ipaddress"]
-  end
-  search(:node, "recipes:*\\:\\:realtrans-vp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
-    vpnames << worker["ipaddress"]
-  end
-  search(:node, "recipes:*\\:\\:realtrans-reg AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
-    regnames << worker["ipaddress"]
-  end
-  fpnames = fpnames.sort.uniq
-  rpnames = rpnames.sort.uniq
-  vpnames = vpnames.sort.uniq
-  regnames = regnames.sort.uniq
-  template "/etc/httpd/proxy.d/rt-#{environ}.proxy.conf" do
-    source "rt.proxy.conf.erb"
-    owner  "root"
-    group  "root"
-    mode   "0644"
-    notifies :reload, resources(:service => "httpd")
-    variables(
-      :fpworkers => fpnames.sort.uniq,
-      :rpworkers => rpnames.sort.uniq,
-      :vpworkers => vpnames.sort.uniq,
-      :regworkers => regnames.sort.uniq,
-      :vhostName => "#{environ}",
-      :environ => "#{environ}",
-      :serveripallow => serveripallow
-    )
-  end
-  template "/etc/httpd/conf.d/rt-#{environ}.vhost.conf" do
-    source "rtvhost#{ssl}.conf.erb"
-    owner  "root"
-    group  "root"
-    mode   "0644"
-    notifies :reload, resources(:service => "httpd")
-    variables(
-      :vhostName => "#{environ}",
-      :serverName => webName["rt#{environ}"]
-    )
-  end
-  directory "/var/www/html/#{environ}" do
-    owner "root"
-    group "root"
+  rtenvirons.each do |environ|
+    # Loop through list of environments to build workers and pass to the vhost/proxy templates
+    fpnames = []
+    rpnames = []
+    vpnames = []
+    regnames = []
+    search(:node, "recipes:*\\:\\:realtrans-fp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
+      fpnames << worker["ipaddress"]
+    end
+    search(:node, "recipes:*\\:\\:realtrans-rp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
+      rpnames << worker["ipaddress"]
+    end
+    search(:node, "recipes:*\\:\\:realtrans-vp AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
+      vpnames << worker["ipaddress"]
+    end
+    search(:node, "recipes:*\\:\\:realtrans-reg AND chef_environment:#{environ}" || "recipes:*\\:\\:realtrans-server AND chef_environment:#{environ}").each do |worker|
+      regnames << worker["ipaddress"]
+    end
+    fpnames = fpnames.sort.uniq
+    rpnames = rpnames.sort.uniq
+    vpnames = vpnames.sort.uniq
+    regnames = regnames.sort.uniq
+    template "/etc/httpd/proxy.d/rt-#{environ}.proxy.conf" do
+      source "rt.proxy.conf.erb"
+      owner  "root"
+      group  "root"
+      mode   "0644"
+      notifies :reload, resources(:service => "httpd")
+      variables(
+        :fpworkers => fpnames.sort.uniq,
+        :rpworkers => rpnames.sort.uniq,
+        :vpworkers => vpnames.sort.uniq,
+        :regworkers => regnames.sort.uniq,
+        :vhostName => "#{environ}",
+        :environ => "#{environ}",
+        :serveripallow => serveripallow
+      )
+    end
+    template "/etc/httpd/conf.d/rt-#{environ}.vhost.conf" do
+      source "rtvhost#{ssl}.conf.erb"
+      owner  "root"
+      group  "root"
+      mode   "0644"
+      notifies :reload, resources(:service => "httpd")
+      variables(
+        :vhostName => "#{environ}",
+        :serverName => webName["rt#{environ}"]
+      )
+    end
+    directory "/var/www/html/#{environ}" do
+      owner "root"
+      group "root"
+    end
   end
 end
