cba9700213cadacd97a4bbeed824e36699c93c9c
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-13 13:04:41 -0400

    updated to try to strip empty or blank elements from array.

--- cookbooks/apache/recipes/rtvhost.rb
+++ cookbooks/apache/recipes/rtvhost.rb
@@ -27,7 +27,7 @@ else
   serveripallow = webName['serveripallow'].split("|")
 
   # Convert the hash list of environments into a string, unique values, then split
-  rtenvirons = rtenvirons.reject{ |w| w.empty? }
+  rtenvirons = rtenvirons.reject{ |w| w.all? {|elem| elem.nil? || elem.strip.empty?}}
   rtenvirons = rtenvirons.sort.uniq
 
   # Loop through list of environments to build workers and pass to the vhost/proxy templates
