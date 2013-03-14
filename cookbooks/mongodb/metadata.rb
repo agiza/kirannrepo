662efdaecb7fb06c5ab1904ba4280addaa0f89a2
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-14 13:02:08 -0400

    Updated to include a depends on iptables.

--- cookbooks/mongodb/metadata.rb
+++ cookbooks/mongodb/metadata.rb
@@ -5,4 +5,4 @@ description      "Installs/Configures mongodb"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.1"
 depends "altisource"
-
+depends "iptables"
