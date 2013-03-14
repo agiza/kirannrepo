fafe259dce78d0c2ff1bea6556dbf4d987bdfa4d
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-14 10:20:07 -0400

    Updated to add iptables rule to logs.

--- cookbooks/realdev/recipes/logs.rb
+++ cookbooks/realdev/recipes/logs.rb
@@ -7,6 +7,9 @@
 # All rights reserved - Do Not Redistribute
 #
 include_recipe "infrastructure::selinux"
+include_recipe "iptables::default"
+
+iptables_rule "port_http"
 
 package "httpd" do
   action :upgrade
