cec100a43e72453b0f09754dda0d59081b204f22
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-18 14:42:49 -0400

    Updated to try to fix the definition for search.

--- cookbooks/altisource/definitions/search_provider.rb
+++ cookbooks/altisource/definitions/search_provider.rb
@@ -3,19 +3,21 @@
 # Definition:: search_provider
 #
 
-define :search_provider, :appnames => {}, :port => nil do
-  "#{params[:name]}ip" = []
+define :search_provider, :name => params[:name], :appnames => params[:appnames], :environment => params[:environment], :port => params[:port] do
+ name_attribute = "#{params[:name]}"
+ name_port = "#{params[:port]}"
+ workerip = []
   appnames["#{params[:appnames]}"].split(" ").each do |app|
     search(:node, "recipes:*\\:\\:#{app} AND chef_environment:#{params[:environment]}".each do |worker|
-      "#{params[:name]}ip" << worker["ipaddress"]
+      workerip << worker["ipaddress"]
     end
   end
-  if "#{params[:name]}ip".nil? || "#{params[:name]}ip".empty?
-    Chef::Log.warn("No #{params[:name]} servers found in search.") && "#{params[:name]}ip" = "No servers found."
+  if workerip.nil? || workerip.empty?
+    Chef::Log.warn("No #{params[:name]} servers found in search.") && workerip = "No servers found."
   else
-    params[:name] = "#{params[:name]}ip".sort.first
+    workername = workerip.sort.first
     #params[:port] = params[:port]
   end
-  node.default."#{params[:name]}" = params[:name]
-  node.default."#{params[:port]}" = params[:port]
+  node.default.name_attribute = "#{workerip}"
+  node.default.name_port = "#{params[:port]}"
 end
