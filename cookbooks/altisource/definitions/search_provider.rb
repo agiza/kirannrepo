9e1b1f6126bd2590757c2e6990cbb91674da21cd
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-18 13:00:50 -0400

    Updated to try a first pass at a search_provider definition

---
+++ cookbooks/altisource/definitions/search_provider.rb
@@ -0,0 +1,21 @@
+#
+# Cookbook Name:: altisource
+# Definition:: search_provider
+#
+
+define :search_provider, :enable => true, :appnames => {}, :port => nil do
+  params[:name] = []
+  appnames["#{params[:appnames]}".split(" ").each do |app|
+    search(:node, "recipes:*\\:\\:#{app} AND chef_environment:#{params[:environment]}".each do |worker|
+      params[:name] << worker["ipaddress"]
+    end
+  end
+  if params[:name].nil? || params[:name].empty?
+    Chef::Log.warn("No #{params[:name]} servers found in search.") && params[:name] = "No servers found."
+  else
+    params[:name] = params[:name].sort.first
+    "#{params[:name]}port" = params[:port]}
+  end
+  node.default."#{params[:name]}" = params[:name]
+  node.default."#{params[:port]}" = params[:port]
+end
