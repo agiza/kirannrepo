97f8e1902c88bc7243316144694fcff22ef4e085
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-18 06:21:13 -0400

    Updated to simply try to detect info presence from data bag on ftp server.

--- cookbooks/integration/recipes/int-realresolution.rb
+++ cookbooks/integration/recipes/int-realresolution.rb
@@ -57,11 +57,13 @@ end
 amqpcred = data_bag_item("rabbitmq", "realtrans")
 amqpcred = amqpcred['user'].split("|")
 realsvc = data_bag_item("integration", "realservicing")
-begin
-  realres = data_bag_item("integration", "realresolution#{node.chef_environment}")
-rescue Net::HTTPServerException
-    realres = data_bag_item("integration", "realresolution")
-raise "Error loading realresolution ftp information from data bag."
+realres = data_bag_item("integration", "realresolution#{node.chef_environment}")
+if realres.nil? || realres.empty?
+  realres = data_bag_item("integration", "realresolution")
+  if realres.nil? || realres.empty?
+    Chef::Log.error("Error trying to set realresolution ftp info from integration data bag.")
+  end
+end
 end
 template "/opt/tomcat/conf/#{app_name}.properties" do
   source "#{app_name}.properties.erb"
