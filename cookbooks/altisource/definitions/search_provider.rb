efe87151b89add54d5ec13ceb2e659ca44566cb6
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-18 13:17:37 -0400

    Updated to try to fix the definition.

--- cookbooks/altisource/definitions/search_provider.rb
+++ cookbooks/altisource/definitions/search_provider.rb
@@ -4,18 +4,18 @@
 #
 
 define :search_provider, :appnames => {}, :port => nil do
-  params[:name] = []
+  "#{params[:name]}ip" = []
   appnames["#{params[:appnames]}"].split(" ").each do |app|
     search(:node, "recipes:*\\:\\:#{app} AND chef_environment:#{params[:environment]}".each do |worker|
-      params[:name] << worker["ipaddress"]
+      "#{params[:name]}ip" << worker["ipaddress"]
     end
   end
-  if params[:name].nil? || params[:name].empty?
-    Chef::Log.warn("No #{params[:name]} servers found in search.") && params[:name] = "No servers found."
+  if "#{params[:name]}ip".nil? || "#{params[:name]}ip".empty?
+    Chef::Log.warn("No #{params[:name]} servers found in search.") && "#{params[:name]}ip" = "No servers found."
   else
-    params[:name] = params[:name].sort.first
-    params[:port] = params[:port]
+    params[:name] = "#{params[:name]}ip".sort.first
+    #params[:port] = params[:port]
   end
-  node.default.params[:name] = params[:name]
-  node.default.params[:port] = params[:port]
+  node.default."#{params[:name]}" = params[:name]
+  node.default."#{params[:port]}" = params[:port]
 end
