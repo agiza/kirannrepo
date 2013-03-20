c3d77463d032ee2ce383e322ec837aba1de29c9c
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-20 08:39:52 -0400

    Updated to add error handling for mysql data bag.

--- cookbooks/realfoundation/recipes/realfoundation.rb
+++ cookbooks/realfoundation/recipes/realfoundation.rb
@@ -16,7 +16,7 @@ else
   if app_version.nil? || app_version.empty? || app_version == "0.0.0-1"
     new_version = search(:node, "#{version_str}:* AND chef_environment:#{node.chef_environment}")
     if new_version.nil? || new_version.empty?
-      Chef::Log.fatal("No version for #{app_name} software package found.")
+      Chef::Log.info("No version for #{app_name} software package found.")
     else
       version_string = []
       new_version.each do |version|
@@ -47,7 +47,7 @@ end
 yum_package "#{app_name}" do
   version "#{app_version}"
   if node.attribute?('package_noinstall') || version == "0.0.0-1"
-    Chef::Log.info("Package is set to not be installed for version is still invalid default.")
+    Chef::Log.info("Package is set to not be installed or version is still default.")
     action :nothing
   else
     action :install
@@ -76,11 +76,12 @@ template "/opt/tomcat/conf/realfoundation.properties" do
   )
 end
 
-mysql = Chef::DataBag.load("infrastructure")
-if mysql["mysqldb#{node.chef_environment}"]
+begin
   mysqldb = data_bag_item("infrastructure", "mysqldb#{node.chef_environment}")
-else
-  mysqldb = data_bag_item("infrastructure", "mysqldb")
+    rescue Net::HTTPServerException
+      mysqldb = data_bag_item("infrastructure", "mysqldb")
+        rescue Net::HTTPServerException
+          raise "Error loading mysqldb information from infrastructure data bag."
 end
 template "/opt/tomcat/conf/Catalina/localhost/realfoundation.xml" do
   source "realfoundation.xml.erb"
