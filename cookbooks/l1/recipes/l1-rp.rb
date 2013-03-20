af788f7c82802d6a6b1a7ec554dc781cebf9fd09
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-20 08:37:16 -0400

    Updated to fix the info message on version is default.

--- cookbooks/l1/recipes/l1-rp.rb
+++ cookbooks/l1/recipes/l1-rp.rb
@@ -44,7 +44,7 @@ end
 yum_package "#{app_name}" do
   version "#{app_version}"
   if node.attribute?('package_noinstall') || version == "0.0.0-1"
-    Chef::Log.info("Package is set to not be installed for version is still invalid default.")
+    Chef::Log.info("Package is set to not be installed or version is still default.")
     action :nothing
   else
     action :install
