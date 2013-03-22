a9d92ef8af26eddc1094a50bed7a937724367723
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-22 16:49:12 -0400

    Updated to add loggin to server search.

--- cookbooks/altisource/definitions/server_search.rb
+++ cookbooks/altisource/definitions/server_search.rb
@@ -6,14 +6,18 @@
 define :server_search do
   
   target = "#{params[:name]}"
+  Chef::Log.info("target is #{target}")
   targetnames = "#{params[:targetnames]}"
+  Chef::Log.info("targetnames is #{targetnames}")
   workerip = []
   targetnames.split(" ").each do |app|
     search(:node, "recipes:*\\:\\:#{app} AND chef_environment:#{params[:environment]}").each do |worker|
+    Chef::Log.info("Search was for #{app} in environment #{params[:environment]} and found #{worker["ipaddress"]})"
       workerip << worker["ipaddress"]
     end
   end
   workerip = workerip.sort.first
+  Chef::Log.info("node entry is for #{workerip}")
   node.default.target = workerip
   Chef::Log.info("node default for #{target} was set to #{workerip}")
 end
