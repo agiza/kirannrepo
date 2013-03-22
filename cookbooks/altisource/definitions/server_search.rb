27a52cfe17513aa1643585bba36f5738f8881d95
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-22 16:55:33 -0400

    Updated to try to pass the targetnames correctly.

--- cookbooks/altisource/definitions/server_search.rb
+++ cookbooks/altisource/definitions/server_search.rb
@@ -7,10 +7,10 @@ define :server_search do
   
   target = "#{params[:name]}"
   Chef::Log.info("target is #{target}")
-  targetnames = "#{params[:targetnames]}"
-  Chef::Log.info("targetnames is #{targetnames}")
+  #targetnames = "#{params[:targetnames]}"
+  Chef::Log.info("targetnames is #{params[:targetnames]}")
   workerip = []
-  targetnames.split(" ").each do |app|
+  params[:targetnames].split(" ").each do |app|
     search(:node, "recipes:*\\:\\:#{app} AND chef_environment:#{params[:environment]}").each do |worker|
     Chef::Log.info("Search was for #{app} in environment #{params[:environment]} and found #{worker["ipaddress"]}")
       workerip << worker["ipaddress"]
