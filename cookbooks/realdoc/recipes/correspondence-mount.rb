d0c8fbc633db25e458c93e826049c3e8dcef01bd
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-22 11:19:27 -0400

    Updated to convert correspondence-mount to definition.

--- cookbooks/realdoc/recipes/correspondence-mount.rb
+++ cookbooks/realdoc/recipes/correspondence-mount.rb
@@ -7,27 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 
-if node.attribute?("novolume")
-  Chef::Log.info("No volume mount attribute is set.")
+corrmount = node[:corrmount]
+if corrmount.nil? || corrmount.empty?
+  Chef::Log.info("No correspondence mounts found to mount.")
 else
-  corrmount = node[:corrmount]
-  if corrmount.nil? || corrmount.empty?
-    Chef::Log.info("No correspondence mounts found to mount.")
-  else
-    execute "corr-mount" do
-      command "/usr/local/bin/corr-mount.sh"
-      action :nothing
-    end
-    template "/usr/local/bin/corr-mount.sh" do
-      source "correspondence-create.erb"
-      group  "root"
-      owner  "root"
-      mode   "0755"
-      variables(
-        :corrmount => corrmount
-      )
-      notifies :run, "execute[corr-mount]", :immediately
-    end
+  include_recipe "altisource::volume"
+  netvolume_mount "volume_corr" do
+    volumes = "#{node[:corrmount]}"
   end
 end
-
