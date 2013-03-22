39091ff9fac28bf934c18606b074c3571c6e279c
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-22 09:56:06 -0400

    Updated to move volume mount from volgrp recipe to mount_volume definition.

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -6,6 +6,11 @@
 #
 # All rights reserved - Do Not Redistribute
 #
-node.default.volumes = "sdb|data|u02/mysqldata1/data|defaults sdc|innodb|u02/mysqldata1/innodb|defaults sdd|mysqllog|u02/mysqldata/mysqllog|defaults sde|tmp|u02/mysqldata/tmp|defaults sdf|backup|u03/mysqldata|defaults"
-include_recipe "altisource::volgrp"
+#node.default.volumes = "sdb|data|u02/mysqldata1/data|defaults sdc|innodb|u02/mysqldata1/innodb|defaults sdd|mysqllog|u02/mysqldata/mysqllog|defaults sde|tmp|u02/mysqldata/tmp|defaults sdf|backup|u03/mysqldata|defaults"
+#include_recipe "altisource::volgrp"
+
+include_recipe "altisource::volume"
+volume_mount "volume_mysql" do
+  volumes "sdb|data|u02/mysqldata1/data|defaults sdc|innodb|u02/mysqldata1/innodb|defaults sdd|mysqllog|u02/mysqldata/mysqllog|defaults sde|tmp|u02/mysqldata/tmp|defaults sdf|backup|u03/mysqldata|defaults"
+end
 
