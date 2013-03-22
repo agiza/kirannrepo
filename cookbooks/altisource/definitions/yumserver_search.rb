d58e731625b22dfe777d24405785f9fd5c2000b8
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-22 13:17:34 -0400

    Updated to try to redefine yumserver search as a definition.

---
+++ cookbooks/altisource/definitions/yumserver_search.rb
@@ -0,0 +1,19 @@
+#
+# Cookbook Name:: altisource
+# Definition:: yumserver_search
+#
+
+define :yumserver_search do
+
+  if node.attribute?("yum_server")
+       yumserver = node[:yum_server]
+  else
+    yumserver = []
+    %w{yumserver yum-repo}.each do |app|
+      search(:node, "recipes:*\\:\\:#{app}").each do |worker|
+        yumserver << worker["ipaddress"]
+      end
+    end
+    yumserver = yumserver.first
+end
+
