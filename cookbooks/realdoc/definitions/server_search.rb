3d78518b35f801076190e065b996be6530a75ea6
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-22 15:44:38 -0400

    Updated to try to move this to the realdoc cookbook.

---
+++ cookbooks/realdoc/definitions/server_search.rb
@@ -0,0 +1,25 @@
+#
+# Cookbook Name:: altisource
+# Definition:: server_search
+#
+
+define :server_search do
+  
+  target = "#{params[:name]}"
+  targetname = "#{params[:target]}"
+  environment = "#{params[:environment]}"
+  begin
+    appdata = data_bag_item("infrastructure", "applications")
+      rescue Net::HTTPServerException
+        raise "No application names found in infrastructure data bag."
+  end
+  target = []
+  appnames = appdata["appnames"]["#{targetname}"].split(" ").each do |app|
+    search(:node, "recipes:*\\:\\:#{app} AND chef_environment:#{environment}").each do |worker|
+      target << worker["ipaddress"]
+    end
+  end
+  target = target.sort.first
+  node.default.target = target
+end
+
