0557ae335221ba83da24e34a622a8427e347ad77
rtnextgen User <rtnextgen@chef.altidev.com>
2013-03-22 09:33:40 -0400

    Updated to add the novolume check into volume_mount definition.

--- cookbooks/altisource/definitions/volume_mount.rb
+++ cookbooks/altisource/definitions/volume_mount.rb
@@ -4,19 +4,22 @@
 #
 
 define :volume_mount do
-
-  execute "volume-mount" do
-    command "/usr/local/bin/#{params[:name]}.sh"
-    action :nothing
-  end
+  if node.attribute?("novolume")
+    Chef::Log.info("No volume mount attribute is set.")
+  else
+    execute "volume-mount" do
+      command "/usr/local/bin/#{params[:name]}.sh"
+      action :nothing
+    end
   
-  template "/usr/local/bin/#{params[:name]}.sh" do
-    source "#{params[:name]}.erb"
-    mode 0755
-    group "root"
-    owner "root"
-    variables( :voldata => "#{params[:volumes]}" )
-    notifies :run, "execute[volume-mount]", :immediately
+    template "/usr/local/bin/#{params[:name]}.sh" do
+      source "#{params[:name]}.erb"
+      mode 0755
+      group "root"
+      owner "root"
+      variables( :voldata => "#{params[:volumes]}" )
+      notifies :run, "execute[volume-mount]", :immediately
+    end
   end
 end
 
