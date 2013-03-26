1daec3a22bc1b577c3800173c4f9398d86f35e1a
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-26 14:57:32 -0400

    Updated to remove cis-mount as we are using volumes.

--- cookbooks/realdoc/recipes/cis-mount.rb
+++ cookbooks/realdoc/recipes/cis-mount.rb
@@ -9,16 +9,6 @@
 
 # cis-mount recipe requires three attributes.  cismount which is the mount point, cisusername which is the smb username to use for credentials and cispassword which is the password.  Failure to provide all three will result in an error.
 
-unless node[:cismount].nil? || node[:cismount].empty?
-  cismount = node[:cismount]
-  include_recipe "altisource::volume"
-  netvolume_mount "volume_cis" do
-    volumes "#{node[:cismount]}"
-  end
-else
-  Chef::Log.info("No cismount information.")
-end
-
 unless node[:cisvolume].nil? || node[:cisvolume].empty?
   cisvolume = node[:cisvolume]
   include_recipe "altisource::volume"
