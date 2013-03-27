2a5fb10547364cf2a28848e375f742d9920fcd83
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-27 09:01:07 -0400

    Updated to try to use the new lvm mount definition.

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -16,6 +16,8 @@ lvm_mount "data" do
   device "/dev/sdb"
   group  "data_vg"
   volume "lvol0"
+  filesystem "ext4"
+  options "defaults"
   mountpoint "/u02/mysqldata1/data"
 end
 
@@ -23,6 +25,8 @@ lvm_mount "innodb" do
   device "/dev/sdc"
   group  "innodb_vg"
   volume "lvol0"
+  filesystem "ext4"
+  options "defaults"
   mountpoint "/u02/mysqldata1/innodb"
 end
 
@@ -30,6 +34,8 @@ lvm_mount "mysqllog" do
   device "/dev/sdd"
   group  "mysqllog_vg"
   volume "lvol0"
+  filesystem "ext4"
+  options "defaults"
   mountpoint "/u02/mysqldata/mysqllog"
 end
 
@@ -37,6 +43,8 @@ lvm_mount "tmp" do
   device "/dev/sde"
   group  "tmp_vg"
   volume "lvol0"
+  filesystem "ext4"
+  options "defaults"
   mountpoint "/u02/mysqldata/tmp"
 end
 
@@ -44,6 +52,8 @@ lvm_mount "backup" do
   device "/dev/sdf"
   group  "backup_vg"
   volume "lvol0"
+  filesystem "ext4"
+  options "defaults"
   mountpoint "/u02/mysqldata"
 end
 
