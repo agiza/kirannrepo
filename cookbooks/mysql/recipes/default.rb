2171cd2109ab0190b15a7aab52a1115fab9bfd51
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-27 07:47:47 -0400

    Updated to try to use the lvm_mount definition instead.

--- cookbooks/mysql/recipes/default.rb
+++ cookbooks/mysql/recipes/default.rb
@@ -8,7 +8,42 @@
 #
 
 include_recipe "altisource::volume"
-volume_mount "volume_mysql" do
-  volumes "sdb|data|u02/mysqldata1/data|defaults sdc|innodb|u02/mysqldata1/innodb|defaults sdd|mysqllog|u02/mysqldata/mysqllog|defaults sde|tmp|u02/mysqldata/tmp|defaults sdf|backup|u03/mysqldata|defaults"
+#volume_mount "volume_mysql" do
+#  volumes "sdb|data|u02/mysqldata1/data|defaults sdc|innodb|u02/mysqldata1/innodb|defaults sdd|mysqllog|u02/mysqldata/mysqllog|defaults sde|tmp|u02/mysqldata/tmp|defaults sdf|backup|u03/mysqldata|defaults"
+#end
+
+lvm_mount "data" do
+  device "/dev/sdb"
+  group "data_vg"
+  volume "lvol0"
+  mountpoint "/u02/mysqldata1/data"
 end
 
+lvm_mount "innodb" do
+  device "/dev/sdc"
+  group  "innodb_vg"
+  volume "lvol0"
+  mountpoint "/u02/mysqldata1/innodb"
+end
+
+lvm_mount "mysqllog" do
+  device "/dev/sdd"
+  group  "mysqllog_vg"
+  volume "lvol0"
+  mountpoint "/u02/mysqldata/mysqllog"
+end
+
+lvm_mount "tmp" do
+  device "/dev/sde"
+  group  "tmp_vg"
+  volume "lvol0"
+  mountpoint "/u02/mysqldata/tmp"
+end
+
+lvm_mount "backup" do
+  device "/dev/sdf"
+  group  "backup_vg"
+  volume "lvol0"
+  mountpoint "/u03/mysqldata"
+end
+  
