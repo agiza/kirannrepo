9e61e31a2c551e5b9f0cb81008a71fc2561eee54
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-27 09:29:44 -0400

    Updated to fix the lvm_mount

--- cookbooks/altisource/definitions/lvm_mount.rb
+++ cookbooks/altisource/definitions/lvm_mount.rb
@@ -8,30 +8,30 @@ define :lvm_mount do
     Chef::Log.info("No volume mount attribute is set.")
   else
     execute "pvcreate" do
-      command "pvcreate #{params[:device]}"
+      command "/sbin/pvcreate #{params[:device]}"
       Chef::Log.info("Would execute pvcreate  #{params[:device]}")
-      only_if "pvdisplay #{params[:device]} 2>&1 | grep -q 'Failed to read physical volume'"
+      only_if "/sbin/pvdisplay #{params[:device]} 2>&1 | grep -q 'Failed to read physical volume'"
       action :run
     end
   
     execute "vgcreate" do
-      command "vgcreate #{params[:group]}"
+      command "/sbin/vgcreate #{params[:group]}"
       Chef::Log.info("Would execute vgcreate #{params[:group]}")
-      only_if "lvdisplay #{params[:group]} 2>&1 | grep -q 'not found'"
+      only_if "/sbin/vgdisplay #{params[:group]} 2>&1 | grep -q 'not found'"
       action :run
     end
 
     execute "lvcreate" do
-      command "lvcreate -l 100%VG #{params[:group]}"
+      command "/sbin/lvcreate -l 100%VG #{params[:group]}"
       Chef::Log.info("Would execute lvcreate -l 100%VG #{params[:group]}")
-      not_if "lvdisplay -c /dev/#{params[:group]}/#{params[:volume]}"
+      not_if "/sbin/lvdisplay -c /dev/#{params[:group]}/#{params[:volume]}"
       action :run
     end
 
     execute "format" do
-      command "mkfs -t #{params[:filesystem]} -m 1 /dev/mapper/#{params[:group]}-#{params[:volume]}"
+      command "/sbin/mkfs -t #{params[:filesystem]} -m 1 /dev/mapper/#{params[:group]}-#{params[:volume]}"
       Chef::Log.info("Would execute mkfs -t #{params[:filesystem]} -m 1 /dev/mapper/#{params[:group]}-#{params[:volume]}")
-      not_if "blkid /dev/mapper/#{params[:group]}-#{params[:volume]} 2>&1 | grep #{params[:filesystem]}"
+      not_if "/sbin/blkid /dev/mapper/#{params[:group]}-#{params[:volume]} | grep #{params[:filesystem]}"
       action :run
     end  
 
