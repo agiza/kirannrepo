4f3730df5372c3ea2a3d12c65579023fe660af24
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-27 07:48:05 -0400

    Updated the definition to include physical volumes.

--- cookbooks/altisource/definitions/lvm_mount.rb
+++ cookbooks/altisource/definitions/lvm_mount.rb
@@ -8,11 +8,14 @@ define :lvm_mount do
     Chef::Log.info("No volume mount attribute is set.")
   else
     include_recipe "lvm::default"
-    lvm_logical_volume "#{params[:name]}" do
-      group "#{params[:group]}"
-      size "100%VG"
-      filesystem "ext4"
-      mount_point "#{params[:mountpoint]}"
+    lvm_volume_group "#{params[:group]}" do
+      physical_volumes [ params[:device] ]
+      lvm_logical_volume "#{params[:volume]}" do
+        group "#{params[:group]}"
+        size "100%VG"
+        filesystem "ext4"
+        mount_point :location => "#{params[:mountpoint]}", :options => "defaults"
+      end
     end
   
 end
