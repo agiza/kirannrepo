407a0abfbf9fd081e5c71f2fcf1543d38957f76f
Bryan Phinney <bryan.phinney@altisource.com>
2013-03-27 09:01:57 -0400

    Updated to turn the actions on.

--- cookbooks/altisource/definitions/lvm_mount.rb
+++ cookbooks/altisource/definitions/lvm_mount.rb
@@ -10,25 +10,25 @@ define :lvm_mount do
     execute "pvcreate" do
       command "pvcreate #{params[:device]}"
       only_if "pvdisplay #{params[:device]} 2>&1 | grep -q 'Failed to read physical volume'"
-      action :nothing
+      action :run
     end
   
     execute "vgcreate" do
       command "vgcreate #{params[:group]}"
       only_if "lvdisplay #{params[:group]} 2>&1 | grep -q 'not found'"
-      action :nothing
+      action :run
     end
 
     execute "lvcreate" do
       command "lvcreate -l 100%VG #{params[:group]}"
       not_if "lvdisplay -c /dev/#{params[:group]}/#{params[:volume]}"
-      action :nothing
+      action :run
     end
 
     execute "format" do
       command "mkfs -t #{params[:filesystem]} -m 1 /dev/mapper/#{params[:group]}-#{params[:volume]}"
       not_if "blkid #{params[:device]} 2>&1 | grep -q #{params[:filesystem]}"
-      action :nothing
+      action :run
     end  
 
     mount "#{params[:name]}" do
