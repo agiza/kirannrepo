69bf0ae457fab0962a195c58fd43c527c5f93335
Bryan Phinney <bryan.phinney@altisource.com>
2013-04-24 13:23:25 -0400

    Updated to convert list into better syntax.

--- cookbooks/apache/recipes/rtvhost.rb
+++ cookbooks/apache/recipes/rtvhost.rb
@@ -7,10 +7,10 @@
 # All rights reserved - Do Not Redistribute
 #
 
-appnames = "realtrans-fp realtrans-rp realtrans-reg realtrans-vp realtrans-server"
+appnames = %w(realtrans-fp realtrans-rp realtrans-reg realtrans-vp realtrans-server)
 # Create an array of all environments with realtrans workers installed
 rtenvirons = []
-appnames.split(" ").each do |app|
+appnames.each do |app|
   #Chef::Log.info("working on #{app}")
   search(:node, "recipes:*\\:\\:#{app}").each do |node|
     #Chef::Log.info("found #{node}")
