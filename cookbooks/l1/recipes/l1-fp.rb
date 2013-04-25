d8923e21a71a64548bb26b600ef5da7aa8771882
Bryan Phinney <bryan.phinney@altisource.com>
2013-04-25 09:19:52 -0400

    Updated to fix the recipe search.

--- cookbooks/l1/recipes/l1-fp.rb
+++ cookbooks/l1/recipes/l1-fp.rb
@@ -14,7 +14,7 @@ if node.attribute?('package_noinstall')
   Chef::Log.info("No version needed.")
 else
   if app_version.nil? || app_version.empty?
-    new_version = search(:node, "recipes:l1\\:\\:#{app_name} AND chef_environment:#{node.chef_environment}")
+    new_version = search(:node, "recipes:*\\:\\:#{app_name} AND chef_environment:#{node.chef_environment}")
     if new_version.nil? || new_version.empty?
       Chef::Log.info("No version for #{app_name} software package found.")
     else
