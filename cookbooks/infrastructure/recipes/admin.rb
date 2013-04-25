c4c7b48bc406813643e2feeeb2dd73935f39c340
Bryan Phinney <bryan.phinney@altisource.com>
2013-04-25 08:56:12 -0400

    Updated to fix the recipe sort.

--- cookbooks/infrastructure/recipes/admin.rb
+++ cookbooks/infrastructure/recipes/admin.rb
@@ -32,12 +32,12 @@ ubuntu_hosts = []
 search(:node, "platform:redhat OR platform:centos").each do |host|
   rhel_hosts << host["hostname"]
 end
-rhel_hosts.uniq.sort!
+rhel_hosts = rhel_hosts.uniq.sort
 
 search(:node, "platform:ubuntu").each do |host|
   ubuntu_hosts << host["hostname"]
 end
-ubuntu_hosts.uniq.sort!
+ubuntu_hosts = ubuntu_hosts.uniq.sort
 
 template "/home/ubuntu/bin/rhel_host" do
   source "rhel_host.erb"
