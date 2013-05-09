587bc157c395f9739806e5abb1d6e53eb1d2c58a
Bryan Phinney <bryan.phinney@altisource.com>
2013-05-09 07:29:55 -0400

    Updated cloudmount recipe to use default settings if there are no attributes set.

--- cookbooks/altisource/recipes/cloudmount.rb
+++ cookbooks/altisource/recipes/cloudmount.rb
@@ -10,7 +10,12 @@ if node.attribute?("cloudmount")
     filesystem "#{node[:cloudmount][:filesystem]}"
     options "#{node[:cloudmount][:options]}"
   end
-else
-  Chef::Log.info("No cloud mount information set.")
+elsif not node.attribute?("cloudmount")
+  Chef::Log.info("No cloud mount information present, attempting to mount default tomcat volume.")
+  cloud_mount "tomcat" do
+    device "/dev/xvdh"
+    mountpoint "/opt/tomcat"
+    filesystem "ext4"
+    options "defaults"
+  end
 end
-
