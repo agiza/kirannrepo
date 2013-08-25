871b27be05517b33a7bac797fbc99d7f02face1b
Carlos Lawton <carlos.lawton@altisource.com>
2013-08-25 19:21:08 -0400

    Added support for running LibreOffice server

---
+++ cookbooks/realdoc/recipes/libreoffice-headless.rb
@@ -0,0 +1,29 @@
+include_recipe "iptables::default"
+iptables_rule "libreoffice-iptables"
+
+%w(libreoffice-headless libreoffice-writer libreoffice-calc libreoffice-draw).each do |pkg|
+  yum_packge pkg do
+    action :upgrade
+  end
+end
+
+(8100..8109).each do |port|
+  template "/etc/libreoffice-#{port}.conf" do
+    source "libreoffice.conf.erb"
+    variables(
+        :port => port
+    )
+  end
+end
+
+(8100..8109).each do |instance|
+  service "libreoffice-#{instance}" do
+    provider Chef::Provider::Service::Upstart
+    source "libreoffice.conf.erb"
+    supports :status => true, :restart => true, :reload => true
+    action [ :enable, :start ]
+  end
+end
+
+
+
