da544787a348614fe4b8d80e0c40c4806ab46905
Carlos Lawton <carlos.lawton@altisource.com>
2013-08-29 21:41:08 -0400

    added realsvc-recon-adapter dependency.  I'm not sure this files should exists.  The recipes should probably be part of run list instead.

--- cookbooks/realdoc/recipes/realdoc-adapter.rb
+++ cookbooks/realdoc/recipes/realdoc-adapter.rb
@@ -10,4 +10,6 @@ include_recipe "realdoc::strongmail-adapter"
 include_recipe "realdoc::rd-transcentra-recon"
 include_recipe "realdoc::rd-mailmerge-adapter"
 include_recipe "realdoc::realservicing-correspondence-adapter"
+include_recipe "realdoc::realsvc-recon-adapter"
+
 
