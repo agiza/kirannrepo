be14ee87b702438ce5b0b38273a4019f94ce2565
Scott McDonald <scott@dnshat.com>
2013-09-11 10:34:11 -0400

    working on jdbc-data-provider.xml.erb

--- cookbooks/realdoc/attributes/default.rb
+++ cookbooks/realdoc/attributes/default.rb
@@ -15,6 +15,7 @@ default["db_valquerytimeout"] = "2"
 # Oracle DB defaults
 default["oradb_server"] = "50.19.6.17"
 default["oradb_port"] = "1521"
+default["validation_query"] = "SELECT 1 from dual"
 # Mongodb defaults
 default["mongodb_database"] = "realdoc"
 default["mongodb_pass"] = "guest"
