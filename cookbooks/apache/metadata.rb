84a6fedc72492e156066def323d349dc800561f4
Brian Scully <brian.scully@altisource.com>
2013-10-16 15:28:10 -0400

    REALTRANS-327: Missed version bump in metadata.rb

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "bryan.phinney@altisource.com"
 license          "All rights reserved"
 description      "Installs/Configures apache"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.3"
+version          "0.0.4"
 depends		 "iptables"
