6e4fa9ea57d6e185e798218f90465827ec2b238d
Carlos Lawton <carlos.lawton@altisource.com>
2013-10-17 13:50:35 -0400

    Added nested database configuration.  I am gradually getting rid of the flat property structure.

--- cookbooks/realdoc/attributes/default.rb
+++ cookbooks/realdoc/attributes/default.rb
@@ -1,68 +1,92 @@
 # Java defaults for Tomcat
-default["java_mem_max"] = "-Xmx1536m"
-default["java_mem_min"] = "-Xms512m"
-default["java_perm_size"] = "-XX:MaxPermSize=768M"
+default['java_mem_max'] = '-Xmx1536m'
+default['java_mem_min'] = '-Xms512m'
+default['java_perm_size'] = '-XX:MaxPermSize=768M'
 # Mysql DB defaults
-default["db_initsize"] = "2"
-default["db_maxactive"] = "10"
-default["db_maxidle"] = "10"
-default["db_maxinactive"] = "10"
-default["db_maxwait"] = "6000"
-default["db_server"] = "mysqldbprim"
-default["db_port"] = "3306"
-default["db_timeevict"] = "60000"
-default["db_valquerytimeout"] = "2"
+default['db_initsize'] = '2'
+default['db_maxactive'] = '10'
+default['db_maxidle'] = '10'
+default['db_maxinactive'] = '10'
+default['db_maxwait'] = '6000'
+default['db_server'] = 'mysqldbprim'
+default['db_port'] = '3306'
+default['db_timeevict'] = '60000'
+default['db_valquerytimeout'] = '2'
 # Oracle DB defaults
-default["oradb_server"] = "50.19.6.17"
-default["oradb_port"] = "1521"
-default["validation_query"] = "SELECT 1 from dual"
+default['oradb_server'] = '50.19.6.17'
+default['oradb_port'] = '1521'
+default['validation_query'] = 'SELECT 1 from dual'
 # Mongodb defaults
-default["mongodb_database"] = "realdoc"
-default["mongodb_pass"] = ""
-default["mongodb_user"] = ""
+default['mongodb_database'] = 'realdoc'
+default['mongodb_pass'] = ''
+default['mongodb_user'] = ''
 # Realfoundation Default attributes
-default["tenantid"] = "TENANT1"
-default["rf_ldap_flag"] = "false"
-default["rf_dao_flag"] = "true"
-default["access_index"] = "realdoc"
-default["log4j_index"] = "realdoc"
+default['tenantid'] = 'TENANT1'
+default['rf_ldap_flag'] = 'false'
+default['rf_dao_flag'] = 'true'
+default['access_index'] = 'realdoc'
+default['log4j_index'] = 'realdoc'
 # Realdoc Default version attributes
-default["realdoc_version"] = "0.0.0-1"
-default["jdbcprov_version"] = "0.0.0-1"
-default["rdcorr_version"] = "0.0.0-1"
-default["rdmailmerge_version"] = "0.0.0-1"
-default["rdproxygen_version"] = "0.0.0-1"
-default["rdtranscentrarecon_version"] = "0.0.0-1"
-default["reconadapter_version"] = "0.0.0-1"
-default["smadapter_version"] = "0.0.0-1"
-default["rdcorradap_version"] = "0.0.0-1"
+default['realdoc_version'] = '0.0.0-1'
+default['jdbcprov_version'] = '0.0.0-1'
+default['rdcorr_version'] = '0.0.0-1'
+default['rdmailmerge_version'] = '0.0.0-1'
+default['rdproxygen_version'] = '0.0.0-1'
+default['rdtranscentrarecon_version'] = '0.0.0-1'
+default['reconadapter_version'] = '0.0.0-1'
+default['smadapter_version'] = '0.0.0-1'
+default['rdcorradap_version'] = '0.0.0-1'
 # Adapters
-default['adapters'] = {
+default[:adapters] = {
     :rs_inbound => {
         :xml_basedir => '${catalina.base}/correspondence',
         :tp2_dir => '${catalina.base}/correspondence/incoming/tp2',
         :io_basedir => '${catalina.base}/correspondence',
-        :done_file_suffix => ".tag",
+        :done_file_suffix => '.tag',
         :xml_poll_freq => 3000
     },
     :rs_outbound => {
         :amqp => {
-            :queue_name => "rd.legacy.adapter"
+            :queue_name => 'rd.legacy.adapter'
         },
         :ftp => {
-            :host => "",
+            :host => '',
             :port => 22,
-            :username => "",
-            :key_file => "",
-            :output_dir => "",
-            :use => "false"
+            :username => '',
+            :key_file => '',
+            :output_dir => '',
+            :use => 'false'
         },
         :output => {
-            :done_file_suffix => ".tag",
+            :done_file_suffix => '.tag',
             :directory => '${catalina.base}/correspondence/recon',
-            :suppression_filename => "realdoc-email-suppression.txt",
-            :text_filename => "realdoc-reconfile.txt",
-            :blacklist_filename => "realdoc-blacklist.txt"
+            :suppression_filename => 'realdoc-email-suppression.txt',
+            :text_filename => 'realdoc-reconfile.txt',
+            :blacklist_filename => 'realdoc-blacklist.txt'
         }
+    }
+}
+# nested database configuration
+default[:dbs] = {
+    :mysql => {
+        :type => 'mysql',
+        :host => 'mysqldbprim',
+        :port => '3306',
+        :initial_size => '2',
+        :max_active => '10',
+        :max_idle => '10',
+        :max_inactive => '10',
+        :max_wait => '6000',
+        :eviction_interval => '60000',
+        :validation_timeout => '2',
+        :username => '',
+        :password => ''
+    },
+    :oracle => {
+        :type => 'oracle',
+        :host => '50.19.6.17',
+        :port => '1521',
+        :username => '',
+        :password => ''
     }
 }
