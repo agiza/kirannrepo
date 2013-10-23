097cb5fd84387fdaf5010374e99d1ca140edb9da
Scott McDonald <scott@dnshat.com>
2013-10-23 14:39:17 -0400

    Fix missing depends - BTS

--- cookbooks/atlassian/metadata.rb
+++ cookbooks/atlassian/metadata.rb
@@ -4,6 +4,6 @@ license          "All rights reserved"
 description      "Installs/Configures atlassian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.2"
-%w{infrastructure altisource}.each do |cookbook|
+%w{infrastructure altisource rvm}.each do |cookbook|
   depends cookbook
 end
