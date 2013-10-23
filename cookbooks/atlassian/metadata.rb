c8422120657de90e8704d6789d33c77d185a7e72
Scott McDonald <scott@dnshat.com>
2013-10-23 10:16:10 -0400

    Missed metadata bump on atlassian cookbook - BTS

--- cookbooks/atlassian/metadata.rb
+++ cookbooks/atlassian/metadata.rb
@@ -3,6 +3,7 @@ maintainer_email "bryan.phinney@altisource.com"
 license          "All rights reserved"
 description      "Installs/Configures atlassian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.1"
-depends		 "infrastructure"
-depends		 "altisource"
+version          "0.0.2"
+%w{infrastructure altisource} do |cookbook|
+  depends cookbook
+end
