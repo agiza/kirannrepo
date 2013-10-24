65eee3d78949979c247ef7d2481ce68d9b2be173
Brian Scully <brian.scully@altisource.com>
2013-10-24 10:14:41 -0400

    Add chef to default gemset for bamboo

--- cookbooks/atlassian/attributes/default.rb
+++ cookbooks/atlassian/attributes/default.rb
@@ -2,5 +2,6 @@ default[:rvm][:default_ruby] = "ruby-1.9.3"
 default[:rvm][:user_default_ruby] = "ruby-1.9.3"
 default[:rvm][:global_gems] = [
 	{ name: 'bundler' },
-	{ name: 'rake' , version: '10.1.0'}
+	{ name: 'rake' , version: '10.1.0'},
+	{ name: 'chef' , version: '11.4.4'}
 ]
