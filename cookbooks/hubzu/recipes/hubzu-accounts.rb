6e40158e5e17e42dcc23a81a4fc65945cd15e7a9
Scott McDonald <scott@dnshat.com>
2013-11-11 12:36:20 -0500

    DEVOPS-94: finished parameterizing a new template file for hubzu-accounts

--- cookbooks/hubzu/recipes/hubzu-accounts.rb
+++ cookbooks/hubzu/recipes/hubzu-accounts.rb
@@ -7,49 +7,11 @@
 # All rights reserved - Do Not Redistribute
 #
 app_name = "hubzu-accounts"
-app_version = node[:hubzuac_version]
-
-if node.attribute?('package_noinstall')
-  Chef::Log.info("No version needed.")
-else
-  if app_version.nil? || app_version.empty? || app_version == "0.0.0-1"
-    new_version = search(:node, "recipes:hubzu\\:\\:#{app_name} AND chef_environment:#{node.chef_environment}")
-    if new_version.nil? || new_version.empty? || new_version == "0.0.0-1"
-      Chef::Log.info("No version for #{app_name} software package found.")
-    else
-      new_version = new_version.first
-      app_version = new_version[:hubzuac_version]
-      node.set[:hubzuac_version] = app_version
-    end
-  else
-    Chef::Log.info("Found version attribute.")
-  end
-end
-
-include_recipe "hubzu::default"
-
-service "altitomcat" do
-  supports :stop => true, :start => true, :restart => true, :reload => true
-  action :nothing
-end
-
-yum_package "#{app_name}" do
-  version "#{app_version}"
-  if node.attribute?('package_noinstall') || version == "0.0.0-1"
-    Chef::Log.info("Package is set to not be installed for version is still invalid default.")
-    action :nothing
-  else
-    action :install
-  end
-  flush_cache [ :before ]
-  allow_downgrade true
-  notifies :restart, resources(:service => "altitomcat")
-end
 
 begin
       accounts = data_bag_item("hubzu", "accounts#{node.chef_environment}")
         rescue Net::HTTPServerException
-           raise "Unable to find hubzu accounts environment databag."
+           raise "Unable to find hubzu-accounts environment specifc databag."
 end
 
 template "/opt/tomcat/conf/#{app_name}.properties" do
