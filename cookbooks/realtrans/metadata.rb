2dbf08c9ba9ed8e8c861041b21dfc50f58abdb84
Brian Scully <brian.scully@altisource.com>
2013-11-13 15:19:54 -0500

    QB-654 RFNG-273 - Bump metadata version

--- cookbooks/realtrans/metadata.rb
+++ cookbooks/realtrans/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "john.mcdonald@altisource.com"
 license          "All rights reserved"
 description      "Installs/Configures realtrans"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.5"
+version          "0.0.6"
 depends		 "altisource"
