113bd4eccb430ff66337d002b3df01eeee369b33
Scott McDonald <scott@dnshat.com>
2013-11-26 11:57:44 -0500

    DEVOPS-96: working on stash.rb recipe

--- cookbooks/infrastructure/recipes/stash.rb
+++ cookbooks/infrastructure/recipes/stash.rb
@@ -12,9 +12,9 @@ app_name = "stash"
 #
 #
 #
-set[:java][:install_flavor]="oracle" 
-set[:java][:accept_oracle_download_terms]=true 
-set[:java][:jdk_version]='7'
+node.set[:java][:install_flavor]="oracle" 
+node.set[:java][:accept_oracle_download_terms]=true 
+node.set[:java][:jdk_version]='7'
 include_recipe "java::oracle"
 
 #include_recipe "mysql::server"
