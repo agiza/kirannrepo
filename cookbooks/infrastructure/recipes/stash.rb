015e942b483d2ae249c743f666a70f8c949908b0
Scott McDonald <scott@dnshat.com>
2013-11-26 10:49:32 -0500

    DEVOPS-96: adding a stash recipe to install pre-reqs

--- cookbooks/infrastructure/recipes/stash.rb
+++ cookbooks/infrastructure/recipes/stash.rb
@@ -11,15 +11,8 @@
 app_name = "stash"
 #
 #
-## JSM: install base set of packages if not installed already
 #
-#%w{telnet lynx traceroute}.each do |pkg|
-#  package pkg do
-#    action :upgrade
-#  end
-#end
-#
-#
+include_recipe "infrastructure::base"
 include_recipe "java::oracle"
-include_recipe "mysql::server"
-include_recipe "mysql::client"
+#include_recipe "mysql::server"
+#include_recipe "mysql::client"
