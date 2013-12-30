e5f76df431b6c43aa6634b48261afb01a2941950
Chef Server rtnextgen <shared@altidev.net>
2013-12-30 08:08:08 -0500

    DEVOPS-277: I think including git and java in metatadata.rb causes chef server to lock up resolving cookbooks same thing happened in infrastructure with those 2 cookbooks added

--- cookbooks/atlassian/metadata.rb
+++ cookbooks/atlassian/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "john.mcdonald@altisource.com"
 license          "All rights reserved"
 description      "Installs/Configures atlassian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.6"
-%w{infrastructure iptables altisource rvm nodejs git java}.each do |cookbook|
+version          "0.0.7"
+%w{infrastructure iptables altisource rvm nodejs}.each do |cookbook|
   depends cookbook
 end
