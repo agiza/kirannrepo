9e29c9edcf6e0d36e6844b9b3085ffac3a3e3f38
Chef Server rtnextgen <shared@altidev.net>
2013-12-30 08:44:49 -0500

    DEVOPS-277: JSM trying to determine if cookbook resolution hangs are a result of git cookbook java cookbook or if it only happens with both enabled

--- cookbooks/atlassian/metadata.rb
+++ cookbooks/atlassian/metadata.rb
@@ -4,6 +4,6 @@ license          "All rights reserved"
 description      "Installs/Configures atlassian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          "0.0.7"
-%w{infrastructure iptables altisource rvm nodejs}.each do |cookbook|
+%w{infrastructure iptables altisource rvm nodejs git}.each do |cookbook|
   depends cookbook
 end
