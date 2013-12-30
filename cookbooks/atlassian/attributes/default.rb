ca8f33830e530fecb3b5a6b5827e3b8c34fe4fae
Chef Server rtnextgen <shared@altidev.net>
2013-12-30 12:04:26 -0500

    DEVOPS-277: [Scott] per discussion with Scully to fix ruby version syntax issues

--- cookbooks/atlassian/attributes/default.rb
+++ cookbooks/atlassian/attributes/default.rb
@@ -1,7 +1,7 @@
 default[:rvm][:default_ruby] = "ruby-1.9.3"
 default[:rvm][:user_default_ruby] = "ruby-1.9.3"
 default[:rvm][:global_gems] = [
-	{ name: 'bundler', version: '1.3.5' },
-	{ name: 'rake' , version: '10.1.0'},
-	{ name: 'chef' , version: '11.4.4'}
+	{ :name => 'bundler', :version => '1.3.5' },
+	{ :name => 'rake', :version => '10.1.0'},
+	{ :name => 'chef', :version => '11.4.4'}
 ]
