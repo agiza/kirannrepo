14a8311edb249edc512ac670db9f842615e53aa2
Chef Server rtnextgen <shared@altidev.net>
2014-01-13 12:03:59 -0500

    DEVOPS-311: update welcome email links to atlas.altidev.net URLs

--- cookbooks/atlassian/metadata.rb
+++ cookbooks/atlassian/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "john.mcdonald@altisource.com"
 license          "All rights reserved"
 description      "Installs/Configures atlassian"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.10"
+version          "0.0.11"
 %w{infrastructure iptables altisource rvm nodejs}.each do |cookbook|
   depends cookbook
 end
