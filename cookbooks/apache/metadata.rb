e4fc7f088f356de0513e3b9f9be5487789664dab
Chef Server rtnextgen <shared@altidev.net>
2014-01-15 14:33:32 -0500

    DEVOPS-321: create system to share large files on Confluence

--- cookbooks/apache/metadata.rb
+++ cookbooks/apache/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "bryan.phinney@altisource.com"
 license          "All rights reserved"
 description      "Installs/Configures apache"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.5"
+version          "0.0.6"
 %w{iptables altisource}.each do |cb|
   depends cb
 end
