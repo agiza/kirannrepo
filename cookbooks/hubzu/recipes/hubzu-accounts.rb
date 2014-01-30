8bdc283c499a200fe04246d1f54b4914a2b9b7e2
Chef Server rtnextgen <shared@altidev.net>
2014-01-30 07:12:28 -0500

    DEVOPS-348: adding chef logic for new hubzu-accounts rpm

--- cookbooks/hubzu/recipes/hubzu-accounts.rb
+++ cookbooks/hubzu/recipes/hubzu-accounts.rb
@@ -7,6 +7,51 @@
 # All rights reserved - Do Not Redistribute
 #
 app_name = "hubzu-accounts"
+app_version = node[:hubzuac_version]
+
+if node.attribute?('package_noinstall')
+  Chef::Log.info("No version needed.")
+else
+  if app_version.nil? || app_version.empty? || app_version == "0.0.0-1"
+    new_version = search(:node, "recipes:hubzu\\:\\:#{app_name} AND chef_environment:#{node.chef_environment}")
+    if new_version.nil? || new_version.empty? || new_version == "0.0.0-1"
+      Chef::Log.info("No version for #{app_name} software package found.")
+    else
+      new_version = new_version.first
+      app_version = new_version[:hubzuac_version]
+      node.set[:hubzuac_version] = app_version
+    end
+  else
+    Chef::Log.info("Found version attribute.")
+  end
+end
+
+include_recipe "hubzu::default"
+hzhost = node[:hzhost]
+hzport = node[:hzport]
+amqphost = node[:amqphost]
+amqpport = node[:amqpport]
+rdochost = node[:rdochost]
+rdocport = node[:rdocport]
+
+service "altitomcat" do
+  supports :stop => true, :start => true, :restart => true, :reload => true
+  action :nothing
+end
+
+yum_package "#{app_name}" do
+  version "#{app_version}"
+  if node.attribute?('package_noinstall') || version == "0.0.0-1"
+    Chef::Log.info("Package is set to not be installed for version is still invalid default.")
+    action :nothing
+  else
+    action :install
+  end
+  flush_cache [ :before ]
+  allow_downgrade true
+  notifies :restart, resources(:service => "altitomcat")
+end
+
 
 begin
       accounts = data_bag_item("hubzu", "accounts#{node.chef_environment}")
