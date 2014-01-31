c19a1f6d96fcbc8342141c3cd3788864fb67ac02
Brian Scully <brian.scully@altisource.com>
2014-01-31 13:17:17 -0500

    REALTRANS-1055: Deploy rt-l adapter with the rest

--- cookbooks/integration/recipes/realtrans-int.rb
+++ cookbooks/integration/recipes/realtrans-int.rb
@@ -7,4 +7,5 @@ include_recipe "integration::int-collateralanalytics"
 include_recipe "integration::int-realresolution"
 include_recipe "integration::int-realservicing"
 include_recipe "integration::int-support"
+include_recipe "integration::int-rtlegacy"
 
