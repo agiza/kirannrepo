4af293505031c88af318685b370ae3cf2c1256ed
Chef Server rtnextgen <shared@altidev.net>
2014-03-12 13:50:22 -0400

    DEVOPS-561: hubzu-backoffice.properties file need to be updated with more properties

--- cookbooks/hubzu/metadata.rb
+++ cookbooks/hubzu/metadata.rb
@@ -3,5 +3,5 @@ maintainer_email "john.mcdonald@altisource.com"
 license          "All rights reserved"
 description      "Installs/Configures hubzu"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.12"
+version          "0.0.13"
 depends		 "altisource"
