561f22ede2953d5063e50c5339f049084b916acd
Chef Server rtnextgen <shared@altidev.net>
2014-03-27 10:05:11 -0400

    DEVOPS-605: creating rd_storage_method variable for template settings per environment data bag

--- cookbooks/realdoc/attributes/default.rb
+++ cookbooks/realdoc/attributes/default.rb
@@ -126,4 +126,7 @@ default['rd_corres_delivery'] = '10'
 default['rd_corres_dispatch'] = '20'
 default['rd_corres_batch'] = '10'
 default['rd_corres_recon'] = '10'
+#storage 
+default['rd_storage_method'] = 'GRID_FS'
+
 
