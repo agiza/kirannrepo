59c7911013ae8cae75f2606ec0bf26059dce9740
Chef Server rtnextgen <shared@altidev.net>
2014-04-24 07:45:44 -0400

    RDNG-5122: update chef recipes for CQA and IQA to make dispatch retry config persistent

--- cookbooks/realdoc/attributes/default.rb
+++ cookbooks/realdoc/attributes/default.rb
@@ -126,6 +126,7 @@ default['rd_corres_delivery'] = '10'
 default['rd_corres_dispatch'] = '20'
 default['rd_corres_batch'] = '10'
 default['rd_corres_recon'] = '10'
+default['rd_corres_retry'] = '1'
 #storage 
 default['rd_storage_method'] = 'GRID_FS'
 default['rd_corr_storage_method'] = 'GRID_FS'
