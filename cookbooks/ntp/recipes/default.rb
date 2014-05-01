b5f496880e1f7f10f3b528fc9eb7f92e287e9d16
Leo Lavin <Leo.Lavin@altisource.com>
2014-05-01 10:39:44 -0400

    Changes to ntp cookbook

---
+++ cookbooks/ntp/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: ntp
+# Recipe:: default
+#
+# Copyright 2014, YOUR_COMPANY_NAME
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'ntp' do 
+   action :upgrade
+end
+
+ntpserver = data_bag_item("infrastructure","ntp")
+
+template '/etc/ntp.conf' do 
+   source  'ntp.conf.erb'
+   group 'root'
+   owner 'root'
+   mode  '0644'
+   notifies :restart, resources(:service => "ntpd")
+   variables(
+       :ntpserver => ntpserver,
+   )
+end
+
+service 'ntpd' do
+   action [:enable, :start]
+end
