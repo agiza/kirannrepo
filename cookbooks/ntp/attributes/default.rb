3802156993c3b8e8f71f22f194892fbd78e5a0ba
Chef Server rtnextgen <shared@altidev.net>
2014-05-05 09:07:54 -0400

    ntp cookbook creation

---
+++ cookbooks/ntp/attributes/default.rb
@@ -0,0 +1,3 @@
+#ntp Server Details
+#
+default['ntp_server'] = '123.108.225.6'
--- cookbooks/ntp/recipes/default.rb
+++ cookbooks/ntp/recipes/default.rb
@@ -11,19 +11,11 @@ package 'ntp' do
    action :upgrade
 end
 
-ntpserver = data_bag_item("infrastructure","ntp")
-
 template "/etc/ntp.conf" do 
    source  "ntp.conf.erb"
    group 'root'
    owner 'root'
    mode  '0644'
    notifies :restart, "service[ntpd]"
-   variables(
-       :ntpserver => ntpserver,
-   )
 end
 
-service 'ntpd' do
-   action [:enable, :start]
-end
