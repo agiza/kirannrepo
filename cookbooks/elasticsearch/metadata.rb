7d60a29bc0c46da6dc3403612836b9ecd46804bd
Chef Server rtnextgen <shared@altidev.net>
2014-05-28 19:07:07 +0000

    DEVOPS-928: working out logic for chef script run with variables

--- cookbooks/elasticsearch/metadata.rb
+++ cookbooks/elasticsearch/metadata.rb
@@ -1,6 +1,6 @@
 maintainer       "Altisource"
-maintainer_email "bryan.phinney@altisource.com"
+maintainer_email "leo.lavin@altisource.com"
 license          "All rights reserved"
 description      "Installs/Configures elasticsearch"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.2.0"
+version          "0.5.1"
--- cookbooks/elasticsearch/recipes/server.rb
+++ cookbooks/elasticsearch/recipes/server.rb
@@ -14,15 +14,15 @@ end
 directory "/usr/share/elasticsearch/logs" do
         owner "root"
 	group "root"
-        mode 755
+        mode 00755
         action :create
 end
 
-file "/etc/profile.d/elasticsearch.sh" do
+template "/etc/profile.d/elasticsearch.sh" do
+     source "elasticsearch.sh.erb"
      owner "root"
      group "root"
-     mode  "0644"
-     content "export ES_HOME=/usr/share/elasticsearch"
+     mode  00755
      action :create
 end
 
@@ -56,6 +56,7 @@ template "/etc/elasticsearch/elasticsearch.yml" do
   source "elasticsearch.yml.erb"
   owner  "elasticsearch"
   group  "elasticsearch"
+  mode  00775
   notifies :restart, resources(:service => "elasticsearch")
 end
 
@@ -63,10 +64,81 @@ template "/etc/sysconfig/sysconfig-elasticsearch" do
   source "sysconfig-elasticsearch.erb"
   owner  "root"
   group  "root"
+  mode  00775
   notifies :restart, resources(:service => "elasticsearch")
+end
+
+directory "/usr/share/elasticsearch/indextmp" do
+        owner "root"
+        group "root"
+        mode 00755
+        action :create
+end
+
+template "/usr/share/elasticsearch/indextmp/aliases.json" do
+     source "aliases.json.erb"
+     owner "root"
+     group "root"
+     mode  00755
+     action :create
+end
+
+template "/usr/share/elasticsearch/indextmp/dispatchesIndex.json" do
+     source "dispatchesIndex.json.erb"
+     owner "root"
+     group "root"
+     mode  00755
+     action :create
+end
+
+template "/usr/share/elasticsearch/indextmp/dispatchesMapping.json" do
+     source "dispatchesMapping.json.erb"
+     owner "root"
+     group "root"
+     mode  00755
+     action :create
+end
+
+template "/usr/share/elasticsearch/indextmp/documentsIndex.json" do
+     source "documentsIndex.json.erb"
+     owner "root"
+     group "root"
+     mode  00755
+     action :create
+end
+
+template "/usr/share/elasticsearch/indextmp/documentsMapping.json" do
+     source "documentsMapping.json.erb"
+     owner "root"
+     group "root"
+     mode  00755
+     action :create
+end
+
+template "/usr/share/elasticsearch/indextmp/dropindex.json" do
+     source "dropindex.json.erb"
+     owner "root"
+     group "root"
+     mode  00755
+     action :create
+end
+
+template "/usr/share/elasticsearch/indextmp/elasticSearchRiverIndex.sh" do
+     source "elasticSearchRiverIndex.sh.erb"
+     owner "root"
+     group "root"
+     mode  00755
+     action :create
 end
 
 service "elasticsearch" do
   action [:enable, :start]
+end
+
+print @mongodb_host
+
+execute "elasticSearchRiverIndex" do
+  command "/usr/share/elasticsearch/indextmp/elasticSearchRiverIndex.sh -h localhost -p #{elasticsearch_port} -k #{mongodb_host} -q #{mongodb_port} -d #{mongodb_database} -v #{elasticsearch_index}"
+  action :run
 end
 
