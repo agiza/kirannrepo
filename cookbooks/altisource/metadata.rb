28f61620e8abc264ac9ac4ea5bd4b5d96585f5b0
Chef Server rtnextgen <shared@altidev.net>
2014-06-04 12:47:46 +0000

    RFNG app install

--- cookbooks/altisource/metadata.rb
+++ cookbooks/altisource/metadata.rb
@@ -3,7 +3,7 @@ maintainer_email "bryan.phinney@altisource.com"
 license          "All rights reserved"
 description      "Installs/Configures altisource"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "0.0.4"
+version          "0.0.5"
 
 %w{ infrastructure iptables }.each do |dep|
 	depends dep
