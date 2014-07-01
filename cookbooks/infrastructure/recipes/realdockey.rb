c0ee6f6164fc81c2cff7ab9dcdf198fe281907d8
Chef Server rtnextgen <shared@altidev.net>
2014-07-01 17:34:45 +0000

    cleanup

---
+++ cookbooks/infrastructure/recipes/realdockey.rb
@@ -0,0 +1,14 @@
+#
+# Cookbook Name:: infrastructure
+# Recipe:: realdockey
+#
+# Copyright 2012, Altisource
+#
+# All rights reserved - Do Not Redistribute
+#
+
+if platform_family?("ubuntu")
+  authkey "realdoc" do
+  end
+end
+
