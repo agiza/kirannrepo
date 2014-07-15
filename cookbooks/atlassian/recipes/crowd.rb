e0430b56d0907a0fee4b37a3e4e74aeda9ddc2b6
Chef Server rtnextgen <shared@altidev.net>
2014-07-15 19:03:50 +0000

    working on crowd

--- cookbooks/atlassian/recipes/crowd.rb
+++ cookbooks/atlassian/recipes/crowd.rb
@@ -7,35 +7,35 @@
 # All rights reserved - Do Not Redistribute
 #
 
-template "/etc/init.d/crowd" do
-  source "crowd-init.erb"
-  owner  "root"
-  group  "root"
-  mode   "0755"
-end
+#template "/etc/init.d/crowd" do
+#  source "crowd-init.erb"
+#  owner  "root"
+#  group  "root"
+#  mode   "0755"
+#end
 
-service "crowd" do
-  supports :start => true, :stop => true, :restart => true, :status => true
-  action :nothing
-end
+#service "crowd" do
+#  supports :start => true, :stop => true, :restart => true, :status => true
+#  action :nothing
+#end
 
-template "/opt/atlassian/crowd/crowd.cfg.xml" do
-  source "crowd.cfg.xml.erb"
-  owner  "root"
-  group  "root"
-  mode   "0644"
-  notifies :restart, resources(:service => "crowd")
-end
+#template "/opt/atlassian/crowd/crowd.cfg.xml" do
+#  source "crowd.cfg.xml.erb"
+#  owner  "root"
+#  group  "root"
+#  mode   "0644"
+#  notifies :restart, resources(:service => "crowd")
+#end
 
-template "/opt/atlassian/crowd/crowd-openidserver-webapp/WEB-INF/classes/crowd.properties" do
-  source "crowd.properties.erb"
-  owner  "root"
-  group  "root"
-  mode   "0644"
-  notifies :restart, resources(:service => "crowd")
-end
+#template "/opt/atlassian/crowd/crowd-openidserver-webapp/WEB-INF/classes/crowd.properties" do
+#  source "crowd.properties.erb"
+#  owner  "root"
+#  group  "root"
+#  mode   "0644"
+#  notifies :restart, resources(:service => "crowd")
+#end
 
-remote_file "/opt/atlassian/crowd/lib/mysql-connector-java-5.1.31-bin.jar" do
+remote_file "/opt/atlassian/crowd/apache-tomcat/lib/mysql-connector-java-5.1.31-bin.jar" do
   source "http://10.0.0.20/yum/common/mysql-connector-java-5.1.31-bin.jar"
   mode  "0644"
   owner "root"
@@ -43,8 +43,8 @@ remote_file "/opt/atlassian/crowd/lib/mysql-connector-java-5.1.31-bin.jar" do
   action :create_if_missing
 end
 
-service "crowd" do
-  supports :start => true, :stop => true, :restart => true, :status => true
-  action :start
-end
+#service "crowd" do
+#  supports :start => true, :stop => true, :restart => true, :status => true
+#  action :start
+#end
 
