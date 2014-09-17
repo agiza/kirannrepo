99a035124e89210178bf37ea6431ee11568941b6
Cosmin Vasii <cosmin.vasii@endava.com>
2014-09-17 18:11:18 +0300

    add tomcat-all cookbook

---
+++ cookbooks/tomcat-all/attributes/default.rb
@@ -0,0 +1,14 @@
+# O.S. related settings
+default['tomcat-all']['user'] = 'tomcat'
+default['tomcat-all']['group'] = 'tomcat'
+
+# Tomcat related settings
+## installation
+default['tomcat-all']['version'] = '7.0.53'
+default['tomcat-all']['install_directory'] = '/opt'
+## configuration
+default['tomcat-all']['shutdown_port'] = '8005'
+default['tomcat-all']['port'] = '8080'
+default['tomcat-all']['max_threads'] = '100'
+default['tomcat-all']['min_spare_threads'] = '10'
+default['tomcat-all']['java_opts'] = '-Xmx3g -XX:PermSize=512M -XX:MaxPermSize=1024m -d64 -server -Djava.awt.headless=true'
