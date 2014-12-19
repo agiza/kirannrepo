9dffffdee28ae31dba843ac1b0492f8f771f1362
Marius Bogdanescu <Marius.Bogdanescu@endava.com>
2014-12-19 11:20:24 -0500

    RRM-9093 Add cookbook for rf-rm-web with shibboleth configuration

---
+++ cookbooks/rf-rm-web/metadata.rb
@@ -0,0 +1,10 @@
+name             'rf-rm-web'
+maintainer       'Altisource Labs'
+maintainer_email 'naor.pinto@altisource.com'
+license          'All rights reserved'
+description      'Installs/Configures rf-apache-shib-sp'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.1'
+
+depends 'altisource'
+depends  'shibboleth-sp'
